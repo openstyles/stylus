15407c4cdea4e049f62d2041e87797b654dbe299
Jason <jason.barnabe@gmail.com>
2016-03-17 21:52:46 -0500

    Add Google Analytics

---
+++ analytics.js
@@ -0,0 +1,9 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-8246384-4']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
