openstyles/stylus

commit 7084dd1223236a19f1039f5b00d5632b1f40d82f

createStyleElement speedup: reuse the template

manage.html
@@ -25,7 +25,10 @@
           </svg>
         </a>
       </h2>
-      <p class="applies-to"><label i18n-text="appliesDisplay"></label></p>
+      <p class="applies-to">
+        <label i18n-text="appliesDisplay"></label>
+        <span class="targets"></span>
+      </p>
       <p class="actions">
         <a class="style-edit-link" href="edit.html?id=">
           <button i18n-text="editStyleLabel"></button>
@@ -89,6 +92,7 @@
       </p>
       <div class="applies-to">
         <div class="targets"></div>
+        <span class="expander">...</span>
       </div>
     </div>
   </template>
@@ -109,10 +113,6 @@
     <details class="applies-to-extra">
       <summary i18n-html="appliesDisplayTruncatedSuffix"></summary>
     </details>
-  </template>
-
-  <template data-id="expandAppliesTo">
-    <span class="expander">...</span>
   </template>
 
   <script src="dom.js"></script>

manage.js
@@ -95,9 +95,8 @@ function showStyles(styles = []) {
     .sort((a, b) => (a.name < b.name ? -1 : a.name == b.name ? 0 : 1));
   const shouldRenderAll = (history.state || {}).scrollY > window.innerHeight;
   const renderBin = document.createDocumentFragment();
-  tDocLoader.stop();
   renderStyles(0);
-  // TODO: remember how many styles fit one page to display just that portion first next time
+
   function renderStyles(index) {
     const t0 = performance.now();
     while (index < sorted.length) {
@@ -116,40 +115,57 @@ function showStyles(styles = []) {
     } else if (shouldRenderAll && 'scrollY' in (history.state || {})) {
       setTimeout(() => scrollTo(0, history.state.scrollY));
     }
+    if (newUI.enabled && newUI.favicons) {
+      debounce(handleEvent.loadFavicons, 16);
+    }
   }
 }
 
 
 function createStyleElement({style, name}) {
-  const entry = template[`style${newUI.enabled ? 'Compact' : ''}`].cloneNode(true);
-  entry.className += ' ' +
-    (style.enabled ? 'enabled' : 'disabled') +
-    (style.updateUrl ? ' updatable' : '');
-  entry.id = 'style-' + style.id;
-  entry.styleId = style.id;
-  entry.styleNameLowerCase = name || style.name.toLocaleLowerCase();
-
-  const editLink = $('.style-name-link', entry);
-  editLink.appendChild(document.createTextNode(style.name));
-  editLink.href = editLink.getAttribute('href') + style.id;
-
-  const homepage = $('.homepage', entry);
-  if (style.url) {
-    homepage.href = homepage.title = style.url;
-  } else {
-    homepage.remove();
+  // query the sub-elements just once, then reuse the references
+  if ((createStyleElement.parts || {}).newUI !== newUI.enabled) {
+    const entry = template[`style${newUI.enabled ? 'Compact' : ''}`].cloneNode(true);
+    createStyleElement.parts = {
+      newUI: newUI.enabled,
+      entry,
+      entryClassBase: entry.className,
+      checker: $('.checker', entry),
+      nameLink: $('.style-name-link', entry),
+      editLink: $('.style-edit-link', entry) || {},
+      editHrefBase: $('.style-name-link, .style-edit-link', entry).getAttribute('href'),
+      homepage: $('.homepage', entry),
+      appliesTo: $('.applies-to', entry),
+      targets: $('.targets', entry),
+      expander: $('.expander', entry),
+      decorations: {
+        urlPrefixesAfter: '*',
+        regexpsBefore: '/',
+        regexpsAfter: '/',
+      },
+    };
   }
+  const parts = createStyleElement.parts;
+  Object.assign(parts.entry, {
+    className: parts.entryClassBase + ' ' +
+      (style.enabled ? 'enabled' : 'disabled') +
+      (style.updateUrl ? ' updatable' : ''),
+    id: 'style-' + style.id,
+    styleId: style.id,
+    styleNameLowerCase: name || style.name.toLocaleLowerCase(),
+  });
 
-  const appliesTo = $('.applies-to', entry);
-  const decorations = {
-    urlPrefixesAfter: '*',
-    regexpsBefore: '/',
-    regexpsAfter: '/',
-  };
-  const displayed = new Set();
-  let container = newUI.enabled ? $('.targets', appliesTo) : appliesTo;
+  parts.nameLink.textContent = style.name;
+  parts.nameLink.href = parts.editLink.href = parts.editHrefBase + style.id;
+  parts.homepage.href = parts.homepage.title = style.url || '';
+
+  // .targets may be a large list so we clone it separately
+  // and paste into the cloned entry in the end
+  const targets = parts.targets.cloneNode(true);
+  let container = targets;
   let numTargets = 0;
   let numIcons = 0;
+  const displayed = new Set();
   for (const type of TARGET_TYPES) {
     for (const section of style.sections) {
       for (const targetValue of section[type] || []) {
@@ -160,7 +176,7 @@ function createStyleElement({style, name}) {
         const element = template.appliesToTarget.cloneNode(true);
         if (!newUI.enabled) {
           if (numTargets == 10) {
-            container = appliesTo.appendChild(template.extraAppliesTo.cloneNode(true));
+            container = container.appendChild(template.extraAppliesTo.cloneNode(true));
           } else if (numTargets > 1) {
             container.appendChild(template.appliesToSeparator.cloneNode(true));
           }
@@ -181,33 +197,33 @@ function createStyleElement({style, name}) {
         }
         element.appendChild(
           document.createTextNode(
-            (decorations[type + 'Before'] || '') +
+            (parts.decorations[type + 'Before'] || '') +
             targetValue +
-            (decorations[type + 'After'] || '')));
+            (parts.decorations[type + 'After'] || '')));
         container.appendChild(element);
         numTargets++;
       }
     }
   }
-  if (!numTargets) {
-    appliesTo.appendChild(template.appliesToEverything.cloneNode(true));
-    entry.classList.add('global');
-  }
 
   if (newUI.enabled) {
-    $('.checker', entry).checked = style.enabled;
-    if (numTargets > newUI.targets) {
-      appliesTo.appendChild(template.expandAppliesTo.cloneNode(true));
-    }
-    if (numIcons) {
+    parts.checker.checked = style.enabled;
+    parts.appliesTo.classList.toggle('has-more', numTargets > newUI.targets);
+    // name is supplied by showStyles so we let it decide when to load the icons
+    if (numIcons && !name) {
       debounce(handleEvent.loadFavicons);
     }
-  } else {
-    const editLink = $('.style-edit-link', entry);
-    editLink.href = editLink.getAttribute('href') + style.id;
   }
 
-  return entry;
+  const newEntry = parts.entry.cloneNode(true);
+  const newTargets = $('.targets', newEntry);
+  if (numTargets) {
+    newTargets.parentElement.replaceChild(targets, newTargets);
+  } else {
+    newTargets.appendChild(template.appliesToEverything.cloneNode(true));
+    newEntry.classList.add('global');
+  }
+  return newEntry;
 }
 
 
@@ -378,6 +394,11 @@ function switchUI({styleOnly} = {}) {
   if (!styleOnly && (stateToggled || missingFavicons)) {
     installed.innerHTML = '';
     getStylesSafe().then(showStyles);
+  } else if (targetsChanged) {
+    for (const targets of $$('.entry .targets')) {
+      const hasMore = targets.children.length > newUI.targets;
+      targets.parentElement.classList.toggle('has-more', hasMore);
+    }
   }
 }
 

manage.css
@@ -78,6 +78,10 @@ a:hover {
   margin-right: 0.1em;
 }
 
+.homepage[href=""] {
+  display: none;
+}
+
 .homepage .svg-icon {
   margin-top: -4px;
   margin-left: .5ex;
@@ -317,6 +321,10 @@ summary {
   line-height: .5ex;
   vertical-align: super;
   letter-spacing: .1ex;
+}
+
+.newUI .applies-to:not(.has-more) .expander {
+  display: none;
 }
 
 .newUI .has-favicons .applies-to .expander {

commit 8e4b2fbbb0c645396d5387a15b9bac7a2ff6cc3b

manage: hardcode edit link; fixes #29 broken in 7084dd12

manage.html
@@ -17,7 +17,7 @@
   <template data-id="style">
     <div class="entry">
       <h2 class="style-name">
-        <a class="style-name-link" href="edit.html?id="></a>
+        <a class="style-name-link"></a>
         <a target="_blank" class="homepage"></a>
       </h2>
       <p class="applies-to">
@@ -25,7 +25,7 @@
         <span class="targets"></span>
       </p>
       <p class="actions">
-        <a class="style-edit-link" href="edit.html?id=">
+        <a class="style-edit-link">
           <button i18n-text="editStyleLabel"></button>
         </a>
         <button class="enable" i18n-text="enableStyleLabel"></button>
@@ -42,7 +42,7 @@
     <div class="entry">
       <h2 class="style-name">
         <input class="checker" type="checkbox" i18n-title="toggleStyle">
-        <a class="style-name-link" href="edit.html?id="></a>
+        <a class="style-name-link"></a>
       </h2>
       <p class="actions">
         <a target="_blank" class="homepage"></a>

manage.js
@@ -155,7 +155,7 @@ function createStyleElement({style, name}) {
       checker: $('.checker', entry) || {},
       nameLink: $('.style-name-link', entry),
       editLink: $('.style-edit-link', entry) || {},
-      editHrefBase: $('.style-name-link', entry).getAttribute('href'),
+      editHrefBase: 'edit.html?id=',
       homepage: $('.homepage', entry),
       homepageIcon: template[`homepageIcon${newUI.enabled ? 'Small' : 'Big'}`],
       appliesTo: $('.applies-to', entry),