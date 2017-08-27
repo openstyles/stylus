543fec82ce2223c8224f84aea11aa7194efd8751
tophf <tophf@gmx.com>
2017-08-27 13:26:48 +0300

    Use our font style for 'dysfunctional' message

--- msgbox/dysfunctional.js
+++ msgbox/dysfunctional.js
@@ -4,3 +4,5 @@ document.body.textContent =
   chrome.i18n.getMessage('dysfunctional');
 document.body.appendChild(document.createElement('div')).textContent =
   chrome.runtime.getURL('manifest.json');
+// set hyphenation language
+document.documentElement.setAttribute('lang', chrome.i18n.getUILanguage());
