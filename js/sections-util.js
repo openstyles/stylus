7d340d62dcb7a25a1ccdc6648ab0683afbda917d
eight <eight04@gmail.com>
2018-10-13 15:13:11 +0800

    Change: drop storage.js, some functions are moved to sections-util

--- js/sections-util.js
+++ js/sections-util.js
@@ -1,4 +1,4 @@
-/* exported styleSectionsEqual styleCodeEmpty */
+/* exported styleSectionsEqual styleCodeEmpty calcStyleDigest */
 'use strict';
 
 function styleCodeEmpty(code) {
@@ -63,5 +63,33 @@ function styleSectionsEqual(a, b, {ignoreCode, checkSource} = {}) {
       array1.every(el => array2.includes(el)) &&
       array2.every(el => array1.includes(el))
     );
+  }
+}
+
+function normalizeStyleSections({sections}) {
+  // retain known properties in an arbitrarily predefined order
+  return (sections || []).map(section => ({
+    code: section.code || '',
+    urls: section.urls || [],
+    urlPrefixes: section.urlPrefixes || [],
+    domains: section.domains || [],
+    regexps: section.regexps || [],
+  }));
+}
+
+function calcStyleDigest(style) {
+  const jsonString = style.usercssData ?
+    style.sourceCode : JSON.stringify(normalizeStyleSections(style));
+  const text = new TextEncoder('utf-8').encode(jsonString);
+  return crypto.subtle.digest('SHA-1', text).then(hex);
+
+  function hex(buffer) {
+    const parts = [];
+    const PAD8 = '00000000';
+    const view = new DataView(buffer);
+    for (let i = 0; i < view.byteLength; i += 4) {
+      parts.push((PAD8 + view.getUint32(i).toString(16)).slice(-8));
+    }
+    return parts.join('');
   }
 }
