31d37c88327d2437cd6f759e2a5c11dbfbe69764
Rob Garrison <wowmotty@gmail.com>
2018-11-16 09:33:09 -0600

    Only update originalMd5 for valid styles

--- background/update.js
+++ background/update.js
@@ -156,8 +156,14 @@
         }
         // USO can't handle POST requests for style json
         return download(style.updateUrl, {body: null})
-          // USO may not provide a correctly updated originalMd5 (#555)
-          .then(text => Object.assign(tryJSONparse(text || '{}'), {originalMd5: md5}));
+          .then(text => {
+            const style = tryJSONparse(text);
+            if (style) {
+              // USO may not provide a correctly updated originalMd5 (#555)
+              style.originalMd5 = md5;
+            }
+            return style;
+          });
       });
     }
 
