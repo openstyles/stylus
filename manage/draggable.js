ed07cb8460c8f19874820a8aeb7c46950f5b3bd6
Rob Garrison <wowmotty@gmail.com>
2018-11-25 17:37:08 -0600

    Make entries draggable

---
+++ manage/draggable.js
@@ -0,0 +1,44 @@
+/* global UI $$ */
+'use strict';
+
+// Polyfill for mobile? - https://caniuse.com/#feat=dragndrop
+(() => {
+  let dragged = null;
+
+  function cleanup() {
+    $$('.entry.dragging').forEach(el => {
+      el.classList.remove('dragging');
+    });
+    $('body').classList.remove('dragging');
+  }
+
+  document.addEventListener('dragstart', event => {
+    const el = event.target.closest('.entry');
+    if (el) {
+      dragged = el;
+      el.classList.add('dragging');
+      $('body').classList.add('dragging');
+    }
+  }, false);
+
+  document.addEventListener('dragend', () => {
+    cleanup();
+  });
+
+  document.addEventListener('dragenter', event => {
+    const el = event.target.closest('.entry, .entry-header');
+    if (el && dragged) {
+      // Insert after the target; keeps header at top
+      el.after(dragged);
+    }
+  }, false);
+
+  document.addEventListener('drop', event => {
+    event.preventDefault();
+    cleanup();
+    if (dragged) {
+      dragged = null;
+    }
+  }, false);
+
+})();
