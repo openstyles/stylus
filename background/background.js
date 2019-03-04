744bf01147360c3ae3c8447c74a4241d1fe2069b
Rob Garrison <wowmotty@gmail.com>
2019-03-03 20:42:03 -0600

    Firefox allow CSP UserCSS install from anywhere. See #618 (#623)
    * Firefox allow CSP UserCSS install from anywhere. See #618
    
    * Fix suffix extension

--- background/background.js
+++ background/background.js
@@ -86,8 +86,8 @@ if (FIREFOX) {
   // FF applies page CSP even to content scripts, https://bugzil.la/1267027
   navigatorUtil.onCommitted(webNavUsercssInstallerFF, {
     url: [
-      {hostSuffix: '.githubusercontent.com', urlSuffix: '.user.css'},
-      {hostSuffix: '.githubusercontent.com', urlSuffix: '.user.styl'},
+      {pathSuffix: '.user.css'},
+      {pathSuffix: '.user.styl'},
     ]
   });
   // FF misses some about:blank iframes so we inject our content script explicitly
