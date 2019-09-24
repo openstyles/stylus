7b0b4f085037d6d16cbe2efd50bc738bbda264ad
tophf <tophf@gmx.com>
2019-09-24 17:10:52 +0300

    move deepEqual next to deepCopy

--- edit/applies-to-line-widget.js
+++ edit/applies-to-line-widget.js
@@ -1,5 +1,5 @@
 /* global regExpTester debounce messageBox CodeMirror template colorMimicry msg
-  $ $create t prefs tryCatch */
+  $ $create t prefs tryCatch deepEqual */
 /* exported createAppliesToLineWidget */
 'use strict';
 
@@ -572,30 +572,6 @@ function createAppliesToLineWidget(cm) {
   function unescapeDoubleslash(s) {
     const hasSingleEscapes = /([^\\]|^)\\([^\\]|$)/.test(s);
     return hasSingleEscapes ? s : s.replace(/\\\\/g, '\\');
-  }
-
-  function deepEqual(a, b, ignoredKeys) {
-    if (!a || !b) return a === b;
-    const type = typeof a;
-    if (type !== typeof b) return false;
-    if (type !== 'object') return a === b;
-    if (Array.isArray(a)) {
-      return Array.isArray(b) &&
-             a.length === b.length &&
-             a.every((v, i) => deepEqual(v, b[i], ignoredKeys));
-    }
-    for (const key in a) {
-      if (!Object.hasOwnProperty.call(a, key) ||
-          ignoredKeys && ignoredKeys.includes(key)) continue;
-      if (!Object.hasOwnProperty.call(b, key)) return false;
-      if (!deepEqual(a[key], b[key], ignoredKeys)) return false;
-    }
-    for (const key in b) {
-      if (!Object.hasOwnProperty.call(b, key) ||
-          ignoredKeys && ignoredKeys.includes(key)) continue;
-      if (!Object.hasOwnProperty.call(a, key)) return false;
-    }
-    return true;
   }
 
   function showRegExpTester(item) {
