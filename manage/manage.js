2fc392a155f55885cfb183d8e0db0cd2339c0127
narcolepticinsomniac <therealdoctorgonzo@gmail.com>
2020-04-28 09:58:38 -0400

    Kill sync animations entirely, add ref

--- manage/manage.js
+++ manage/manage.js
@@ -548,7 +548,7 @@ function handleUpdate(style, {reason, method} = {}) {
     handleUpdateInstalled(entry, reason);
   }
   filterAndAppend({entry}).then(sorter.update);
-  if (!entry.matches('.hidden') && reason !== 'import' && !$('#stylus-embedded-options')) {
+  if (!entry.matches('.hidden') && reason !== 'import' && reason !== 'sync') {
     animateElement(entry);
     requestAnimationFrame(() => scrollElementIntoView(entry));
   }
