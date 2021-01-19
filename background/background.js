openstyles/stylus

commit fdbfb235473f63435fb8b852da1b359dcfeddd65

API groups + use executeScript for early injection (#1149)
* parserlib: fast section extraction, tweaks and speedups
* csslint: "simple-not" rule
* csslint: enable and fix "selector-newline" rule
* simplify db: resolve with result
* simplify download()
* remove noCode param as it wastes more time/memory on copying
* styleManager: switch style<->data names to reflect their actual contents
* inline method bodies to avoid indirection and enable better autocomplete/hint/jump support in IDE
* upgrade getEventKeyName to handle mouse clicks
* don't trust location.href as it hides text fragment
* getAllKeys is implemented since Chrome48, FF44
* allow recoverable css errors + async'ify usercss.js
* openManage: unminimize windows
* remove the obsolete Chrome pre-65 workaround
* fix temporal dead zone in apply.js
* ff bug workaround for simple editor window
* consistent window scrolling in scrollToEditor and jumpToPos
* rework waitForSelector and collapsible <details>
* blank paint frame workaround for new Chrome
* extract stuff from edit.js and load on demand
* simplify regexpTester::isShown
* move MozDocMapper to sections-util.js
* extract fitSelectBox()
* initialize router earlier
* use helpPopup.close()
* fix autofocus in popups, follow-up to 5bb1b5ef
* clone objects in prefs.get() + cosmetics
* reuse getAll result for INC

background/background.js
@@ -1,72 +1,105 @@
-/* global download prefs openURL FIREFOX CHROME
-  URLS ignoreChromeError chromeLocal semverCompare
-  styleManager msg navigatorUtil workerUtil contentScripts sync
-  findExistingTab activateTab isTabReplaceable getActiveTab
-*/
-
+/* global API msg */// msg.js
+/* global addAPI bgReady */// common.js
+/* global createWorker */// worker-util.js
+/* global prefs */
+/* global styleMan */
+/* global syncMan */
+/* global updateMan */
+/* global usercssMan */
+/* global
+  FIREFOX
+  URLS
+  activateTab
+  download
+  findExistingTab
+  getActiveTab
+  isTabReplaceable
+  openURL
+*/ // toolbox.js
 'use strict';
 
-// eslint-disable-next-line no-var
-var backgroundWorker = workerUtil.createWorker({
-  url: '/background/background-worker.js',
-});
+//#region API
 
-// eslint-disable-next-line no-var
-var browserCommands, contextMenus;
+addAPI(/** @namespace API */ {
 
-// *************************************************************************
-// browser commands
-browserCommands = {
-  openManage,
-  openOptions: () => openManage({options: true}),
-  styleDisableAll(info) {
-    prefs.set('disableAll', info ? info.checked : !prefs.get('disableAll'));
+  styles: styleMan,
+  sync: syncMan,
+  updater: updateMan,
+  usercss: usercssMan,
+  /** @type {BackgroundWorker} */
+  worker: createWorker({url: '/background/background-worker'}),
+
+  download(url, opts) {
+    return typeof url === 'string' && url.startsWith(URLS.uso) &&
+      this.sender.url.startsWith(URLS.uso) &&
+      download(url, opts || {});
   },
-  reload: () => chrome.runtime.reload(),
-};
 
-window.API_METHODS = Object.assign(window.API_METHODS || {}, {
-  deleteStyle: styleManager.deleteStyle,
-  editSave: styleManager.editSave,
-  findStyle: styleManager.findStyle,
-  getAllStyles: styleManager.getAllStyles, // used by importer
-  getSectionsByUrl: styleManager.getSectionsByUrl,
-  getStyle: styleManager.get,
-  getStylesByUrl: styleManager.getStylesByUrl,
-  importStyle: styleManager.importStyle,
-  importManyStyles: styleManager.importMany,
-  installStyle: styleManager.installStyle,
-  styleExists: styleManager.styleExists,
-  toggleStyle: styleManager.toggleStyle,
-
-  addInclusion: styleManager.addInclusion,
-  removeInclusion: styleManager.removeInclusion,
-  addExclusion: styleManager.addExclusion,
-  removeExclusion: styleManager.removeExclusion,
-
+  /** @returns {string} */
   getTabUrlPrefix() {
-    const {url} = this.sender.tab;
-    if (url.startsWith(URLS.ownOrigin)) {
-      return 'stylus';
+    return this.sender.tab.url.match(/^([\w-]+:\/+[^/#]+)/)[1];
+  },
+
+  /**
+   * Opens the editor or activates an existing tab
+   * @param {{
+       id?: number
+       domain?: string
+       'url-prefix'?: string
+     }} params
+   * @returns {Promise<chrome.tabs.Tab>}
+   */
+  async openEditor(params) {
+    const u = new URL(chrome.runtime.getURL('edit.html'));
+    u.search = new URLSearchParams(params);
+    const wnd = prefs.get('openEditInWindow');
+    const wndPos = wnd && prefs.get('windowPosition');
+    const wndBase = wnd && prefs.get('openEditInWindow.popup') ? {type: 'popup'} : {};
+    const ffBug = wnd && FIREFOX; // https://bugzil.la/1271047
+    const tab = await openURL({
+      url: `${u}`,
+      currentWindow: null,
+      newWindow: Object.assign(wndBase, !ffBug && wndPos),
+    });
+    if (ffBug) await browser.windows.update(tab.windowId, wndPos);
+    return tab;
+  },
+
+  /** @returns {Promise<chrome.tabs.Tab>} */
+  async openManage({options = false, search, searchMode} = {}) {
+    let url = chrome.runtime.getURL('manage.html');
+    if (search) {
+      url += `?search=${encodeURIComponent(search)}&searchMode=${searchMode}`;
     }
-    return url.match(/^([\w-]+:\/+[^/#]+)/)[1];
+    if (options) {
+      url += '#stylus-options';
+    }
+    let tab = await findExistingTab({
+      url,
+      currentWindow: null,
+      ignoreHash: true,
+      ignoreSearch: true,
+    });
+    if (tab) {
+      await activateTab(tab);
+      if (url !== (tab.pendingUrl || tab.url)) {
+        await msg.sendTab(tab.id, {method: 'pushState', url}).catch(console.error);
+      }
+      return tab;
+    }
+    tab = await getActiveTab();
+    return isTabReplaceable(tab, url)
+      ? activateTab(tab, {url})
+      : browser.tabs.create({url}).then(activateTab); // activateTab unminimizes the window
   },
 
-  download(msg) {
-    delete msg.method;
-    return download(msg.url, msg);
-  },
-  parseCss({code}) {
-    return backgroundWorker.parseMozFormat({code});
-  },
-  getPrefs: () => prefs.values,
-  setPref: (key, value) => prefs.set(key, value),
-
-  openEditor,
-
-  /* Same as openURL, the only extra prop in `opts` is `message` - it'll be sent when the tab is ready,
-  which is needed in the popup, otherwise another extension could force the tab to open in foreground
-  thus auto-closing the popup (in Chrome at least) and preventing the sendMessage code from running */
+  /**
+   * Same as openURL, the only extra prop in `opts` is `message` - it'll be sent
+   * when the tab is ready, which is needed in the popup, otherwise another
+   * extension could force the tab to open in foreground thus auto-closing the
+   * popup (in Chrome at least) and preventing the sendMessage code from running
+   * @returns {Promise<chrome.tabs.Tab>}
+   */
   async openURL(opts) {
     const tab = await openURL(opts);
     if (opts.message) {
@@ -87,251 +120,62 @@ window.API_METHODS = Object.assign(window.API_METHODS || {}, {
     }
   },
 
-  optionsCustomizeHotkeys() {
-    return browserCommands.openOptions()
-      .then(() => new Promise(resolve => setTimeout(resolve, 500)))
-      .then(() => msg.broadcastExtension({method: 'optionsCustomizeHotkeys'}));
+  prefs: {
+    getValues: () => prefs.__values, // will be deepCopy'd by apiHandler
+    set: prefs.set,
   },
-
-  syncStart: sync.start,
-  syncStop: sync.stop,
-  syncNow: sync.syncNow,
-  getSyncStatus: sync.getStatus,
-  syncLogin: sync.login,
-
-  openManage,
 });
 
-// *************************************************************************
-// register all listeners
-msg.on(onRuntimeMessage);
+//#endregion
+//#region Events
 
-// tell apply.js to refresh styles for non-committed navigation
-navigatorUtil.onUrlChange(({tabId, frameId}, type) => {
-  if (type !== 'committed') {
-    msg.sendTab(tabId, {method: 'urlChanged'}, {frameId})
-      .catch(msg.ignoreError);
-  }
-});
-
-if (FIREFOX) {
-  // FF misses some about:blank iframes so we inject our content script explicitly
-  navigatorUtil.onDOMContentLoaded(webNavIframeHelperFF, {
-    url: [
-      {urlEquals: 'about:blank'},
-    ],
-  });
-}
-
-if (chrome.contextMenus) {
-  chrome.contextMenus.onClicked.addListener((info, tab) =>
-    contextMenus[info.menuItemId].click(info, tab));
-}
-
-if (chrome.commands) {
-  // Not available in Firefox - https://bugzilla.mozilla.org/show_bug.cgi?id=1240350
-  chrome.commands.onCommand.addListener(command => browserCommands[command]());
-}
-
-// *************************************************************************
-chrome.runtime.onInstalled.addListener(({reason, previousVersion}) => {
-  if (reason !== 'update') return;
-  if (semverCompare(previousVersion, '1.5.13') <= 0) {
-    // Removing unused stuff
-    // TODO: delete this entire block by the middle of 2021
-    try {
-      localStorage.clear();
-    } catch (e) {}
-    setTimeout(async () => {
-      const del = Object.keys(await chromeLocal.get())
-        .filter(key => key.startsWith('usoSearchCache'));
-      if (del.length) chromeLocal.remove(del);
-    }, 15e3);
-  }
-});
-
-// *************************************************************************
-// context menus
-contextMenus = {
-  'show-badge': {
-    title: 'menuShowBadge',
-    click: info => prefs.set(info.menuItemId, info.checked),
-  },
-  'disableAll': {
-    title: 'disableAllStyles',
-    click: browserCommands.styleDisableAll,
-  },
-  'open-manager': {
-    title: 'openStylesManager',
-    click: browserCommands.openManage,
-  },
-  'open-options': {
-    title: 'openOptions',
-    click: browserCommands.openOptions,
-  },
-  'reload': {
-    presentIf: async () => (await browser.management.getSelf()).installType === 'development',
-    title: 'reload',
-    click: browserCommands.reload,
-  },
-  'editor.contextDelete': {
-    presentIf: () => !FIREFOX && prefs.get('editor.contextDelete'),
-    title: 'editDeleteText',
-    type: 'normal',
-    contexts: ['editable'],
-    documentUrlPatterns: [URLS.ownOrigin + 'edit*'],
-    click: (info, tab) => {
-      msg.sendTab(tab.id, {method: 'editDeleteText'}, undefined, 'extension')
-        .catch(msg.ignoreError);
-    },
+const browserCommands = {
+  openManage: () => API.openManage(),
+  openOptions: () => API.openManage({options: true}),
+  reload: () => chrome.runtime.reload(),
+  styleDisableAll(info) {
+    prefs.set('disableAll', info ? info.checked : !prefs.get('disableAll'));
   },
 };
 
-async function createContextMenus(ids) {
-  for (const id of ids) {
-    let item = contextMenus[id];
-    if (item.presentIf && !await item.presentIf()) {
-      continue;
+if (chrome.commands) {
+  chrome.commands.onCommand.addListener(id => browserCommands[id]());
+}
+
+chrome.runtime.onInstalled.addListener(({reason, previousVersion}) => {
+  if (reason === 'update') {
+    const [a, b, c] = (previousVersion || '').split('.');
+    if (a <= 1 && b <= 5 && c <= 13) { // 1.5.13
+      require(['/background/remove-unused-storage']);
     }
-    item = Object.assign({id}, item);
-    delete item.presentIf;
-    item.title = chrome.i18n.getMessage(item.title);
-    if (!item.type && typeof prefs.defaults[id] === 'boolean') {
-      item.type = 'checkbox';
-      item.checked = prefs.get(id);
-    }
-    if (!item.contexts) {
-      item.contexts = ['browser_action'];
-    }
-    delete item.click;
-    chrome.contextMenus.create(item, ignoreChromeError);
   }
-}
+});
 
-if (chrome.contextMenus) {
-  // "Delete" item in context menu for browsers that don't have it
-  if (CHROME &&
-      // looking at the end of UA string
-      /(Vivaldi|Safari)\/[\d.]+$/.test(navigator.userAgent) &&
-      // skip forks with Flash as those are likely to have the menu e.g. CentBrowser
-      !Array.from(navigator.plugins).some(p => p.name === 'Shockwave Flash')) {
-    prefs.defaults['editor.contextDelete'] = true;
+msg.on((msg, sender) => {
+  if (msg.method === 'invokeAPI') {
+    let res = msg.path.reduce((res, name) => res && res[name], API);
+    if (!res) throw new Error(`Unknown API.${msg.path.join('.')}`);
+    res = res.apply({msg, sender}, msg.args);
+    return res === undefined ? null : res;
   }
-  // circumvent the bug with disabling check marks in Chrome 62-64
-  const toggleCheckmark = CHROME >= 62 && CHROME <= 64 ?
-    (id => chrome.contextMenus.remove(id, () => createContextMenus([id]) + ignoreChromeError())) :
-    ((id, checked) => chrome.contextMenus.update(id, {checked}, ignoreChromeError));
+});
 
-  const togglePresence = (id, checked) => {
-    if (checked) {
-      createContextMenus([id]);
-    } else {
-      chrome.contextMenus.remove(id, ignoreChromeError);
-    }
-  };
+//#endregion
 
-  const keys = Object.keys(contextMenus);
-  prefs.subscribe(keys.filter(id => typeof prefs.defaults[id] === 'boolean'), toggleCheckmark);
-  prefs.subscribe(keys.filter(id => contextMenus[id].presentIf && id in prefs.defaults), togglePresence);
-  createContextMenus(keys);
-}
-
-// reinject content scripts when the extension is reloaded/updated. Firefox
-// would handle this automatically.
-if (!FIREFOX) {
-  setTimeout(contentScripts.injectToAllTabs, 0);
-}
-
-// register hotkeys
-if (FIREFOX && browser.commands && browser.commands.update) {
-  const hotkeyPrefs = Object.keys(prefs.defaults).filter(k => k.startsWith('hotkey.'));
-  prefs.subscribe(hotkeyPrefs, (name, value) => {
-    try {
-      name = name.split('.')[1];
-      if (value.trim()) {
-        browser.commands.update({name, shortcut: value});
-      } else {
-        browser.commands.reset(name);
-      }
-    } catch (e) {}
-  });
-}
-
-msg.broadcast({method: 'backgroundReady'});
-
-function webNavIframeHelperFF({tabId, frameId}) {
-  if (!frameId) return;
-  msg.sendTab(tabId, {method: 'ping'}, {frameId})
-    .catch(() => false)
-    .then(pong => {
-      if (pong) return;
-      // insert apply.js to iframe
-      const files = chrome.runtime.getManifest().content_scripts[0].js;
-      for (const file of files) {
-        chrome.tabs.executeScript(tabId, {
-          frameId,
-          file,
-          matchAboutBlank: true,
-        }, ignoreChromeError);
-      }
-    });
-}
-
-function onRuntimeMessage(msg, sender) {
-  if (msg.method !== 'invokeAPI') {
-    return;
-  }
-  const fn = window.API_METHODS[msg.name];
-  if (!fn) {
-    throw new Error(`unknown API: ${msg.name}`);
-  }
-  const res = fn.apply({msg, sender}, msg.args);
-  return res === undefined ? null : res;
-}
-
-function openEditor(params) {
-  /* Open the editor. Activate if it is already opened
-
-  params: {
-    id?: Number,
-    domain?: String,
-    'url-prefix'?: String
-  }
-  */
-  const u = new URL(chrome.runtime.getURL('edit.html'));
-  u.search = new URLSearchParams(params);
-  return openURL({
-    url: `${u}`,
-    currentWindow: null,
-    newWindow: prefs.get('openEditInWindow') && Object.assign({},
-      prefs.get('openEditInWindow.popup') && {type: 'popup'},
-      prefs.get('windowPosition')),
-  });
-}
-
-async function openManage({options = false, search, searchMode} = {}) {
-  let url = chrome.runtime.getURL('manage.html');
-  if (search) {
-    url += `?search=${encodeURIComponent(search)}&searchMode=${searchMode}`;
-  }
-  if (options) {
-    url += '#stylus-options';
-  }
-  let tab = await findExistingTab({
-    url,
-    currentWindow: null,
-    ignoreHash: true,
-    ignoreSearch: true,
-  });
-  if (tab) {
-    await activateTab(tab);
-    if (url !== (tab.pendingUrl || tab.url)) {
-      await msg.sendTab(tab.id, {method: 'pushState', url}).catch(console.error);
-    }
-    return tab;
-  }
-  tab = await getActiveTab();
-  return isTabReplaceable(tab, url)
-    ? activateTab(tab, {url})
-    : browser.tabs.create({url});
-}
+Promise.all([
+  bgReady.styles,
+  /* These are loaded conditionally.
+     Each item uses `require` individually so IDE can jump to the source and track usage. */
+  FIREFOX &&
+    require(['/background/style-via-api']),
+  FIREFOX && ((browser.commands || {}).update) &&
+    require(['/background/browser-cmd-hotkeys']),
+  !FIREFOX &&
+    require(['/background/content-scripts']),
+  chrome.contextMenus &&
+    require(['/background/context-menus']),
+]).then(() => {
+  bgReady._resolveAll();
+  msg.isBgReady = true;
+  msg.broadcast({method: 'backgroundReady'});
+});

commit a88996be6f4ad2e11ddaafd6e69362ff45076a87

restore correct handling of openEditInWindow
regressed in fdbfb235
fixes #1156

background/background.js
@@ -59,7 +59,7 @@ addAPI(/** @namespace API */ {
     const tab = await openURL({
       url: `${u}`,
       currentWindow: null,
-      newWindow: Object.assign(wndBase, !ffBug && wndPos),
+      newWindow: wnd && Object.assign(wndBase, !ffBug && wndPos),
     });
     if (ffBug) await browser.windows.update(tab.windowId, wndPos);
     return tab;