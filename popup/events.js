openstyles/stylus

commit fdbfb235473f63435fb8b852da1b359dcfeddd65

API groups + use executeScript for early injection (#1149)
* parserlib: fast section extraction, tweaks and speedups
* csslint: "simple-not" rule
* csslint: enable and fix "selector-newline" rule
* simplify db: resolve with result
* simplify download()
* remove noCode param as it wastes more time/memory on copying
* styleManager: switch style<->data names to reflect their actual contents
* inline method bodies to avoid indirection and enable better autocomplete/hint/jump support in IDE
* upgrade getEventKeyName to handle mouse clicks
* don't trust location.href as it hides text fragment
* getAllKeys is implemented since Chrome48, FF44
* allow recoverable css errors + async'ify usercss.js
* openManage: unminimize windows
* remove the obsolete Chrome pre-65 workaround
* fix temporal dead zone in apply.js
* ff bug workaround for simple editor window
* consistent window scrolling in scrollToEditor and jumpToPos
* rework waitForSelector and collapsible <details>
* blank paint frame workaround for new Chrome
* extract stuff from edit.js and load on demand
* simplify regexpTester::isShown
* move MozDocMapper to sections-util.js
* extract fitSelectBox()
* initialize router earlier
* use helpPopup.close()
* fix autofocus in popups, follow-up to 5bb1b5ef
* clone objects in prefs.get() + cosmetics
* reuse getAll result for INC

popup/events.js
@@ -1,0 +1,198 @@
+/* global $ $$ $remove animateElement getEventKeyName moveFocus */// dom.js
+/* global API */// msg.js
+/* global getActiveTab tryJSONparse */// toolbox.js
+/* global resortEntries tabURL */// popup.js
+/* global t */// localization.js
+'use strict';
+
+const MODAL_SHOWN = 'data-display'; // attribute name
+
+const Events = {
+
+  async configure(event) {
+    const {styleId, styleIsUsercss} = getClickedStyleElement(event);
+    if (styleIsUsercss) {
+      const [style] = await Promise.all([
+        API.styles.get(styleId),
+        require(['/popup/hotkeys']), /* global hotkeys */
+        require(['/js/dlg/config-dialog']), /* global configDialog */
+      ]);
+      hotkeys.setState(false);
+      await configDialog(style);
+      hotkeys.setState(true);
+    } else {
+      Events.openURLandHide.call(this, event);
+    }
+  },
+
+  copyContent(event) {
+    event.preventDefault();
+    const target = document.activeElement;
+    const message = $('.copy-message');
+    navigator.clipboard.writeText(target.textContent);
+    target.classList.add('copied');
+    message.classList.add('show-message');
+    setTimeout(() => {
+      target.classList.remove('copied');
+      message.classList.remove('show-message');
+    }, 1000);
+  },
+
+  delete(event) {
+    const entry = getClickedStyleElement(event);
+    const box = $('#confirm');
+    box.dataset.id = entry.styleId;
+    $('b', box).textContent = $('.style-name', entry).textContent;
+    Events.showModal(box, '[data-cmd=cancel]');
+  },
+
+  getExcludeRule(type) {
+    const u = new URL(tabURL);
+    return type === 'domain'
+      ? u.origin + '/*'
+      : escapeGlob(u.origin + u.pathname); // current page
+  },
+
+  async hideModal(box, {animate} = {}) {
+    window.off('keydown', box._onkeydown);
+    box._onkeydown = null;
+    if (animate) {
+      box.style.animationName = '';
+      await animateElement(box, 'lights-on');
+    }
+    box.removeAttribute(MODAL_SHOWN);
+  },
+
+  indicator(event) {
+    const entry = getClickedStyleElement(event);
+    const info = t.template.regexpProblemExplanation.cloneNode(true);
+    $remove('#' + info.id);
+    $$('a', info).forEach(el => (el.onclick = Events.openURLandHide));
+    $$('button', info).forEach(el => (el.onclick = closeExplanation));
+    entry.appendChild(info);
+  },
+
+  isStyleExcluded({exclusions}, type) {
+    if (!exclusions) {
+      return false;
+    }
+    const rule = Events.getExcludeRule(type);
+    return exclusions.includes(rule);
+  },
+
+  maybeEdit(event) {
+    if (!(
+      event.button === 0 && (event.ctrlKey || event.metaKey) ||
+      event.button === 1 ||
+      event.button === 2)) {
+      return;
+    }
+    // open an editor on middleclick
+    const el = event.target;
+    if (el.matches('.entry, .style-edit-link') || el.closest('.style-name')) {
+      this.onmouseup = () => $('.style-edit-link', this).click();
+      this.oncontextmenu = event => event.preventDefault();
+      event.preventDefault();
+      return;
+    }
+    // prevent the popup being opened in a background tab
+    // when an irrelevant link was accidentally clicked
+    if (el.closest('a')) {
+      event.preventDefault();
+      return;
+    }
+  },
+
+  name(event) {
+    $('input', this).dispatchEvent(new MouseEvent('click'));
+    event.preventDefault();
+  },
+
+  async openEditor(event, options) {
+    event.preventDefault();
+    await API.openEditor(options);
+    window.close();
+  },
+
+  async openManager(event) {
+    event.preventDefault();
+    const isSearch = tabURL && (event.shiftKey || event.button === 2);
+    await API.openManage(isSearch ? {search: tabURL, searchMode: 'url'} : {});
+    window.close();
+  },
+
+  async openURLandHide(event) {
+    event.preventDefault();
+    await API.openURL({
+      url: this.href || this.dataset.href,
+      index: (await getActiveTab()).index + 1,
+      message: tryJSONparse(this.dataset.sendMessage),
+    });
+    window.close();
+  },
+
+  showModal(box, cancelButtonSelector) {
+    const oldBox = $(`[${MODAL_SHOWN}]`);
+    if (oldBox) box.style.animationName = 'none';
+    // '' would be fine but 'true' is backward-compatible with the existing userstyles
+    box.setAttribute(MODAL_SHOWN, 'true');
+    box._onkeydown = e => {
+      const key = getEventKeyName(e);
+      switch (key) {
+        case 'Tab':
+        case 'Shift-Tab':
+          e.preventDefault();
+          moveFocus(box, e.shiftKey ? -1 : 1);
+          break;
+        case 'Escape': {
+          e.preventDefault();
+          window.onkeydown = null;
+          $(cancelButtonSelector, box).click();
+          break;
+        }
+      }
+    };
+    window.on('keydown', box._onkeydown);
+    moveFocus(box, 0);
+    Events.hideModal(oldBox);
+  },
+
+  async toggleState(event) {
+    // when fired on checkbox, prevent the parent label from seeing the event, see #501
+    event.stopPropagation();
+    await API.styles.toggle((getClickedStyleElement(event) || {}).styleId, this.checked);
+    resortEntries();
+  },
+
+  toggleExclude(event, type) {
+    const entry = getClickedStyleElement(event);
+    if (event.target.checked) {
+      API.styles.addExclusion(entry.styleMeta.id, Events.getExcludeRule(type));
+    } else {
+      API.styles.removeExclusion(entry.styleMeta.id, Events.getExcludeRule(type));
+    }
+  },
+
+  toggleMenu(event) {
+    const entry = getClickedStyleElement(event);
+    const menu = $('.menu', entry);
+    if (menu.hasAttribute(MODAL_SHOWN)) {
+      Events.hideModal(menu, {animate: true});
+    } else {
+      $('.menu-title', entry).textContent = $('.style-name', entry).textContent;
+      Events.showModal(menu, '.menu-close');
+    }
+  },
+};
+
+function closeExplanation() {
+  $('#regexp-explanation').remove();
+}
+
+function escapeGlob(text) {
+  return text.replace(/\*/g, '\\*');
+}
+
+function getClickedStyleElement(event) {
+  return event.target.closest('.entry');
+}

manage/events.js
@@ -1,0 +1,283 @@
+/* global API */// msg.js
+/* global changeQueue installed newUI */// manage.js
+/* global checkUpdate handleUpdateInstalled */// updater-ui.js
+/* global createStyleElement createTargetsElement getFaviconSrc */// render.js
+/* global debounce getOwnTab openURL sessionStore */// toolbox.js
+/* global filterAndAppend showFiltersStats */// filters.js
+/* global sorter */
+/* global t */// localization.js
+/* global
+  $
+  $$
+  $entry
+  animateElement
+  getEventKeyName
+  messageBoxProxy
+  scrollElementIntoView
+*/// dom.js
+'use strict';
+
+const Events = {
+
+  addEntryTitle(link) {
+    const style = link.closest('.entry').styleMeta;
+    const ucd = style.usercssData;
+    link.title =
+      `${t('dateInstalled')}: ${t.formatDate(style.installDate) || '—'}\n` +
+      `${t('dateUpdated')}: ${t.formatDate(style.updateDate) || '—'}\n` +
+      (ucd ? `UserCSS, v.${ucd.version}` : '');
+  },
+
+  check(event, entry) {
+    checkUpdate(entry, {single: true});
+  },
+
+  async config(event, {styleMeta}) {
+    await require(['/js/dlg/config-dialog']); /* global configDialog */
+    configDialog(styleMeta);
+  },
+
+  async delete(event, entry) {
+    const id = entry.styleId;
+    animateElement(entry);
+    const {button} = await messageBoxProxy.show({
+      title: t('deleteStyleConfirm'),
+      contents: entry.styleMeta.customName || entry.styleMeta.name,
+      className: 'danger center',
+      buttons: [t('confirmDelete'), t('confirmCancel')],
+    });
+    if (button === 0) {
+      API.styles.delete(id);
+    }
+    const deleteButton = $('#message-box-buttons > button');
+    if (deleteButton) deleteButton.removeAttribute('data-focused-via-click');
+  },
+
+  async edit(event, entry) {
+    if (event.altKey) {
+      return;
+    }
+    event.preventDefault();
+    event.stopPropagation();
+    const key = getEventKeyName(event);
+    const url = $('[href]', entry).href;
+    const ownTab = await getOwnTab();
+    if (key === 'MouseL') {
+      sessionStore['manageStylesHistory' + ownTab.id] = url;
+      location.href = url;
+    } else if (chrome.windows && key === 'Shift-MouseL') {
+      API.openEditor({id: entry.styleId});
+    } else {
+      openURL({
+        url,
+        index: ownTab.index + 1,
+        active: key === 'Shift-MouseM' || key === 'Shift-Ctrl-MouseL',
+      });
+    }
+  },
+
+  expandTargets(event, entry) {
+    if (!entry._allTargetsRendered) {
+      createTargetsElement({entry, expanded: true});
+      setTimeout(getFaviconSrc, 0, entry);
+    }
+    this.closest('.applies-to').classList.toggle('expanded');
+  },
+
+  async external(event) {
+    // Not handling Shift-click - the built-in 'open in a new window' command
+    if (getEventKeyName(event) !== 'Shift-MouseL') {
+      const {index} = await getOwnTab();
+      openURL({
+        url: event.target.closest('a').href,
+        index: index + 1,
+        active: !event.ctrlKey || event.shiftKey,
+      });
+    }
+  },
+
+  entryClicked(event) {
+    const target = event.target;
+    const entry = target.closest('.entry');
+    for (const selector in Events.ENTRY_ROUTES) {
+      for (let el = target; el && el !== entry; el = el.parentElement) {
+        if (el.matches(selector)) {
+          return Events.ENTRY_ROUTES[selector].call(el, event, entry);
+        }
+      }
+    }
+  },
+
+  lazyAddEntryTitle({type, target}) {
+    const cell = target.closest('h2.style-name, [data-type=age]');
+    if (cell) {
+      const link = $('.style-name-link', cell) || cell;
+      if (type === 'mouseover' && !link.title) {
+        debounce(Events.addEntryTitle, 50, link);
+      } else {
+        debounce.unregister(Events.addEntryTitle);
+      }
+    }
+  },
+
+  name(event, entry) {
+    if (newUI.enabled) Events.edit(event, entry);
+  },
+
+  toggle(event, entry) {
+    API.styles.toggle(entry.styleId, this.matches('.enable') || this.checked);
+  },
+
+  update(event, entry) {
+    const json = entry.updatedCode;
+    json.id = entry.styleId;
+    (json.usercssData ? API.usercss.install : API.styles.install)(json);
+  },
+};
+
+Events.ENTRY_ROUTES = {
+  'input, .enable, .disable': Events.toggle,
+  '.style-name': Events.name,
+  '.homepage': Events.external,
+  '.check-update': Events.check,
+  '.update': Events.update,
+  '.delete': Events.delete,
+  '.applies-to .expander': Events.expandTargets,
+  '.configure-usercss': Events.config,
+};
+
+/* exported handleBulkChange */
+function handleBulkChange() {
+  for (const msg of changeQueue) {
+    const {id} = msg.style;
+    if (msg.method === 'styleDeleted') {
+      handleDelete(id);
+      changeQueue.time = performance.now();
+    } else {
+      handleUpdateForId(id, msg);
+    }
+  }
+  changeQueue.length = 0;
+}
+
+function handleDelete(id) {
+  const node = $entry(id);
+  if (node) {
+    node.remove();
+    if (node.matches('.can-update')) {
+      const btnApply = $('#apply-all-updates');
+      btnApply.dataset.value = Number(btnApply.dataset.value) - 1;
+    }
+    showFiltersStats();
+  }
+}
+
+function handleUpdate(style, {reason, method} = {}) {
+  if (reason === 'editPreview' || reason === 'editPreviewEnd') return;
+  let entry;
+  let oldEntry = $entry(style);
+  if (oldEntry && method === 'styleUpdated') {
+    handleToggledOrCodeOnly();
+  }
+  entry = entry || createStyleElement({style});
+  if (oldEntry) {
+    if (oldEntry.styleNameLowerCase === entry.styleNameLowerCase) {
+      installed.replaceChild(entry, oldEntry);
+    } else {
+      oldEntry.remove();
+    }
+  }
+  if ((reason === 'update' || reason === 'install') && entry.matches('.updatable')) {
+    handleUpdateInstalled(entry, reason);
+  }
+  filterAndAppend({entry}).then(sorter.update);
+  if (!entry.matches('.hidden') && reason !== 'import' && reason !== 'sync') {
+    animateElement(entry);
+    requestAnimationFrame(() => scrollElementIntoView(entry));
+  }
+  getFaviconSrc(entry);
+
+  function handleToggledOrCodeOnly() {
+    style.sections.forEach(s => (s.code = null));
+    style.sourceCode = null;
+    const diff = objectDiff(oldEntry.styleMeta, style)
+      .filter(({key, path}) => path || (!key.startsWith('original') && !key.endsWith('Date')));
+    if (diff.length === 0) {
+      // only code was modified
+      entry = oldEntry;
+      oldEntry = null;
+    }
+    if (diff.length === 1 && diff[0].key === 'enabled') {
+      oldEntry.classList.toggle('enabled', style.enabled);
+      oldEntry.classList.toggle('disabled', !style.enabled);
+      $$('input', oldEntry).forEach(el => (el.checked = style.enabled));
+      oldEntry.styleMeta = style;
+      entry = oldEntry;
+      oldEntry = null;
+    }
+  }
+}
+
+async function handleUpdateForId(id, opts) {
+  handleUpdate(await API.styles.get(id), opts);
+  changeQueue.time = performance.now();
+}
+
+/* exported handleVisibilityChange */
+function handleVisibilityChange() {
+  switch (document.visibilityState) {
+    // page restored without reloading via history navigation (currently only in FF)
+    // the catch here is that DOM may be outdated so we'll at least refresh the just edited style
+    // assuming other changes aren't important enough to justify making a complicated DOM sync
+    case 'visible': {
+      const id = sessionStore.justEditedStyleId;
+      if (id) {
+        handleUpdateForId(Number(id), {method: 'styleUpdated'});
+        delete sessionStore.justEditedStyleId;
+      }
+      break;
+    }
+    // going away
+    case 'hidden':
+      history.replaceState({scrollY: window.scrollY}, document.title);
+      break;
+  }
+}
+
+function objectDiff(first, second, path = '') {
+  const diff = [];
+  for (const key in first) {
+    const a = first[key];
+    const b = second[key];
+    if (a === b) {
+      continue;
+    }
+    if (b === undefined) {
+      diff.push({path, key, values: [a], type: 'removed'});
+      continue;
+    }
+    if (a && typeof a.filter === 'function' && b && typeof b.filter === 'function') {
+      if (
+        a.length !== b.length ||
+        a.some((el, i) => {
+          const result = !el || typeof el !== 'object'
+            ? el !== b[i]
+            : objectDiff(el, b[i], path + key + '[' + i + '].').length;
+          return result;
+        })
+      ) {
+        diff.push({path, key, values: [a, b], type: 'changed'});
+      }
+    } else if (typeof a === 'object' && typeof b === 'object') {
+      diff.push(...objectDiff(a, b, path + key + '.'));
+    } else {
+      diff.push({path, key, values: [a, b], type: 'changed'});
+    }
+  }
+  for (const key in second) {
+    if (!(key in first)) {
+      diff.push({path, key, values: [second[key]], type: 'added'});
+    }
+  }
+  return diff;
+}

commit 75db3601d0f59424e6cf85b8c6746e33f108359d

fix #1177
regressed in fdbfb235

popup/events.js
@@ -154,7 +154,7 @@ const Events = {
     };
     window.on('keydown', box._onkeydown);
     moveFocus(box, 0);
-    Events.hideModal(oldBox);
+    if (oldBox) Events.hideModal(oldBox);
   },
 
   async toggleState(event) {