openstyles/stylus

commit 7a479edc565b237f2e9843e941004e6dcc856eae

more consistent focus/scroll behavior of global search
* keep page scroll position on Esc/close
* focus the match on Enter

edit/global-search.js
@@ -61,8 +61,17 @@ onDOMready().then(() => {
         switch (document.activeElement) {
           case state.input:
             if (state.dialog.dataset.type === 'find') {
-              doSearch({canAdvance: false});
-              destroyDialog();
+              const found = doSearch({canAdvance: false});
+              if (found) {
+                const target = $('.' + TARGET_CLASS);
+                const cm = target.CodeMirror;
+                (cm || target).focus();
+                if (cm) {
+                  const pos = cm.state.search.searchPos;
+                  cm.setSelection(pos.from, pos.to);
+                }
+              }
+              destroyDialog({restoreFocus: !found});
               return;
             }
             // fallthrough
@@ -235,6 +244,7 @@ onDOMready().then(() => {
     } else {
       showTally(0, 0);
     }
+    return found;
   }
 
 
@@ -634,7 +644,12 @@ onDOMready().then(() => {
     $.remove(DIALOG_SELECTOR);
     debounce.unregister(doSearch);
     makeTargetVisible(null);
-    if (restoreFocus) setTimeout(focusNoScroll, 0, state.originalFocus);
+    if (restoreFocus) {
+      setTimeout(focusNoScroll, 0, state.originalFocus);
+    } else {
+      saveWindowScrollPos();
+      restoreWindowScrollPos({immediately: false});
+    }
   }
 
 
@@ -868,8 +883,13 @@ onDOMready().then(() => {
 
 
   function restoreWindowScrollPos({immediately = true} = {}) {
+    if (!immediately) {
+      // run in the next microtask cycle
+      new Promise(() => restoreWindowScrollPos({immediately: true}));
+      return;
+    }
     if (window.scrollX !== state.scrollX || window.scrollY !== state.scrollY) {
-      invokeOrPostpone(immediately, window.scrollTo, 0, state.scrollX, state.scrollY);
+      window.scrollTo(state.scrollX, state.scrollY);
     }
   }
 

commit 2d5788766a5b1d26b9b8cdab18af7fe836cb5794

fix Enter key clearing selected text, fixes #1219
regressed in 7a479edc

edit/global-search.js
@@ -70,7 +70,9 @@
               if (found) {
                 const target = $('.' + TARGET_CLASS);
                 const cm = target.CodeMirror;
-                (cm || target).focus();
+                /* Since this runs in `keydown` event we have to delay focusing
+                 * to prevent CodeMirror from seeing and handling the key */
+                setTimeout(() => (cm || target).focus());
                 if (cm) {
                   const {from, to} = cm.state.search.searchPos;
                   cm.jumpToPos(from, to);