233a000b83edd27874b52ce5dd9761bd8b6d3d2a
tophf <tophf@gmx.com>
2021-04-08 09:09:05 +0300

    remove the unnecessary change

--- content/install-hook-usercss.js
+++ content/install-hook-usercss.js
@@ -5,11 +5,11 @@ if (typeof window.oldCode !== 'string') {
   window.oldCode = (document.querySelector('body > pre') || document.body).textContent;
   chrome.runtime.onConnect.addListener(port => {
     if (port.name !== 'downloadSelf') return;
-    port.onMessage.addListener(async ({id, force, timer}) => {
+    port.onMessage.addListener(async ({id, force}) => {
       const msg = {id};
       try {
         const code = await (await fetch(location.href, {mode: 'same-origin'})).text();
-        if ((code !== window.oldCode || force) || !(timer && code === window.oldCode)) {
+        if (code !== window.oldCode || force) {
           msg.code = window.oldCode = code;
         }
       } catch (error) {
