621e4ce6fd305d5d6a5a65d346d69b7c3c2a1abc
Gusted <williamzijl7@hotmail.com>
2021-05-30 22:20:52 +0200

    Add credentials for production server

--- background/token-manager.js
+++ background/token-manager.js
@@ -50,8 +50,9 @@ const tokenMan = (() => {
     },
     userstylesworld: {
       flow: 'code',
-      clientId: 'publicccc_client',
-      clientSecret: 'secreettUwU', // Don't judege.
+      clientId: 'zeDmKhJIfJqULtcrGMsWaxRtWHEimKgS',
+      clientSecret: 'wqHsvTuThQmXmDiVvOpZxPwSIbyycNFImpAOTxjaIRqDbsXcTOqrymMJKsOMuibFaij' +
+      'ZZAkVYTDbLkQuYFKqgpMsMlFlgwQOYHvHFbgxQHDTwwdOroYhOwFuekCwXUlk',
       authURL: URLS.usw + 'api/oauth/authorize_style',
       tokenURL: URLS.usw + 'api/oauth/access_token',
       redirect_uri: 'https://gusted.xyz/callback_helper/',
