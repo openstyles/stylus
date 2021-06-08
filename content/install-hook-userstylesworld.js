62791cda820a4a00aca1368201dc213b9e0744ad
Gusted <williamzijl7@hotmail.com>
2021-06-08 18:32:01 +0200

    Wait for `usw-ready`before sending style

--- content/install-hook-userstylesworld.js
+++ content/install-hook-userstylesworld.js
@@ -16,14 +16,15 @@
     && allowedOrigin === event.origin
     ) {
       sendPostMessage({type: 'usw-remove-stylus-button'});
+
+      if (window.location.pathname === '/api/oauth/authorize_style/new') {
+        API.styles.find({_linking: true}).then(style => {
+          console.log(style);
+          sendPostMessage({type: 'usw-fill-new-style', data: style});
+        });
+      }
     }
   };
-
-  if (window.location.pathname === '/api/oauth/authorize_style/new') {
-    API.styles.find({_linking: true}).then(style => {
-      sendPostMessage({type: 'usw-fill-new-style', data: style});
-    });
-  }
 
   window.addEventListener('message', onPageLoaded);
 })();
