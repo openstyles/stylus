ede20ca30cbb7a1cf2d61af415841a3cf7e8ade7
Gusted <williamzijl7@hotmail.com>
2021-07-20 14:18:13 +0200

    Don't rely on _isUswLinked
    It will now simply pass the vendor_data query which will exposed on the `location.search` once you are on the `/api/oauth/style/new` path. And thus can be retrieved which styleID should be need to get.
    
    Related USw commit: https://github.com/userstyles-world/userstyles.world/commit/59b2eb8f3afec8b8cf8efc92c7b8d15d1e3b6fe9

--- content/install-hook-userstylesworld.js
+++ content/install-hook-userstylesworld.js
@@ -18,7 +18,12 @@
       sendPostMessage({type: 'usw-remove-stylus-button'});
 
       if (location.pathname === '/api/oauth/style/new') {
-        API.styles.find({_isUswLinked: true}).then(style => {
+        // Gets the search query string, and get the vendor_data from it.
+        // Which is the id of the style that shall be linked with.
+        const vendorData = window.location.search.split('&')
+          .find(query => query.startsWith('vendor_data'))
+          .split('=')[1];
+        API.styles.get(Number(vendorData)).then(style => {
           style.sourceCode = style.tmpSourceCode;
           sendPostMessage({type: 'usw-fill-new-style', data: style});
         });
