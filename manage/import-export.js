openstyles/stylus

commit a2c8953e634deeaaeebb7adb57d4c757903cfc93

Use flex to center #message-box (#1271)
* Use flex to center #message-box

- Resolves #1270
- See details on the issue mentioned.

* Use existing .center

* Remove center rule at non-center selector

manage/import-export.js
@@ -209,6 +209,7 @@ async function importFromString(jsonString) {
       contents: $create('#import', report.length ? report : t('importReportUnchanged')),
       buttons: [t('confirmClose'), numChanged && t('undo')],
       onshow: bindClick,
+      className: 'center',
     })
       .then(({button}) => {
         if (button === 1) {

manage/updater-ui.js
@@ -224,6 +224,7 @@ async function showUpdateHistory(event) {
   messageBoxProxy.show({
     title: t('updateCheckHistory'),
     contents: log,
+    className: 'center',
     blockScroll: true,
     buttons: [
       t('confirmOK'),

commit 91324a4a48d8ea7c2d51fa3671a7e437887d9d79

use `center-dialog` class for import&history
regressed in a2c8953e which centered the contents too

js/dlg/message-box.css
@@ -36,7 +36,8 @@
   animation: fadeout .25s ease-in-out;
 }
 
-#message-box.center {
+#message-box.center,
+#message-box.center-dialog {
   align-items: center;
   justify-content: center;
 }
@@ -49,7 +50,8 @@
   text-align: left;
 }
 
-#message-box.center > div {
+#message-box.center > div,
+#message-box.center-dialog > div {
   top: unset;
   right: unset;
 }

manage/import-export.js
@@ -206,10 +206,10 @@ async function importFromString(jsonString) {
     const report = entries.map(renderStats).filter(Boolean);
     messageBoxProxy.show({
       title: t('importReportTitle'),
+      className: 'center-dialog',
       contents: $create('#import', report.length ? report : t('importReportUnchanged')),
       buttons: [t('confirmClose'), numChanged && t('undo')],
       onshow: bindClick,
-      className: 'center',
     })
       .then(({button}) => {
         if (button === 1) {

manage/updater-ui.js
@@ -223,8 +223,8 @@ async function showUpdateHistory(event) {
   const logText = lines.join('\n');
   messageBoxProxy.show({
     title: t('updateCheckHistory'),
+    className: 'center-dialog',
     contents: log,
-    className: 'center',
     blockScroll: true,
     buttons: [
       t('confirmOK'),