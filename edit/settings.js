b553325d7c5a3f9517283566f5616220d76ed4df
tophf <tophf@gmx.com>
2021-12-29 13:26:10 +0300

    simplify css + save + autosave

--- edit/settings.js
+++ edit/settings.js
@@ -1,90 +1,91 @@
-/* global $ $$ $create moveFocus */// dom.js
+/* global $ $$ moveFocus setupLivePrefs */// dom.js
 /* global API */// msg.js
 /* global editor */
 /* global helpPopup */// util.js
 /* global t */// localization.js
+/* global debounce */// toolbox.js
 /* exported StyleSettings */
 'use strict';
 
 function StyleSettings() {
-  const {style} = editor;
+  const AUTOSAVE_DELAY = 500; // same as config-dialog.js
   const ui = t.template.styleSettings.cloneNode(true);
-  const inputs = [
-    createInput('.style-update-url input', () => style.updateUrl || '',
-      e => API.styles.config(style.id, 'updateUrl', e.target.value)),
-    createRadio('.style-prefer-scheme input', () => style.preferScheme || 'none',
-      e => API.styles.config(style.id, 'preferScheme', e.target.value)),
-    ...[
-      ['.style-include', 'inclusions'],
-      ['.style-exclude', 'exclusions'],
-    ].map(createArea),
+  const elAuto = $('[id="config.autosave"]', ui);
+  const elSave = $('#ss-save', ui);
+  const pendingSetters = new Map();
+  const {style} = editor;
+  const updaters = [
+    initInput('#ss-update-url', () => style.updateUrl || '',
+      val => API.styles.config(style.id, 'updateUrl', val)),
+    initRadio('ss-scheme', () => style.preferScheme || 'none',
+      val => API.styles.config(style.id, 'preferScheme', val)),
+    initArea('inclusions'),
+    initArea('exclusions'),
   ];
   (editor.updateSettings = () => {
-    inputs.forEach(i => i.update());
+    updaters.forEach(fn => fn());
   })();
-  helpPopup.show(t('styleSettings'), $create([
-    ui,
-    $create('.buttons', [
-      $create('button', {
-        onclick: helpPopup.close,
-        title: t('autosaveNotice'),
-      }, t('confirmClose')),
-    ]),
-  ]), {
+  helpPopup.show(t('styleSettings'), ui, {
     className: 'style-settings-popup',
   });
+  elSave.onclick = save;
+  $('#ss-close', ui).onclick = helpPopup.close;
+  setupLivePrefs([elAuto.id]);
   moveFocus(ui, 0);
 
-  function textToList(text) {
-    const list = text.split(/\s*\r?\n\s*/g);
-    return list.filter(Boolean);
+  function autosave(el, setter) {
+    pendingSetters.set(el, setter);
+    helpPopup.div.classList.add('dirty');
+    elSave.disabled = false;
+    if (elAuto.checked) debounce(save, AUTOSAVE_DELAY);
   }
 
-  function createArea([parentSel, type]) {
-    const sel = parentSel + ' textarea';
-    const el = $(sel, ui);
-    el.on('input', () => {
+  function initArea(type) {
+    const selector = `#ss-${type}`;
+    const el = $(selector, ui);
+    el.oninput = () => {
       const val = el.value;
       el.rows = val.match(/^/gm).length + !val.endsWith('\n');
-    });
-    return createInput(sel,
+    };
+    return initInput(selector,
       () => {
         const list = style[type] || [];
         const text = list.join('\n');
         el.rows = (list.length || 1) + 1;
         return text;
       },
-      () => API.styles.config(style.id, type, textToList(el.value))
+      val => API.styles.config(style.id, type, textToList(val))
     );
   }
 
-  function createRadio(selector, getter, setter) {
-    const els = $$(selector, ui);
-    for (const el of els) {
-      el.addEventListener('change', e => {
-        if (el.checked) {
-          setter(e);
-        }
-      });
-    }
-    return {
-      update() {
-        for (const el of els) {
-          if (el.value === getter()) {
-            el.checked = true;
-          }
-        }
-      },
+  function initInput(selector, getter, setter) {
+    const el = $(selector, ui);
+    el.oninput = () => autosave(el, setter);
+    return () => {
+      const val = getter();
+      // Skipping if unchanged to preserve the Undo history of the input
+      if (el.value !== val) el.value = val;
     };
   }
 
-  function createInput(selector, getter, setter) {
-    const el = $(selector, ui);
-    el.addEventListener('change', setter);
-    return {
-      update() {
-        el.value = getter();
-      },
+  function initRadio(name, getter, setter) {
+    for (const el of $$(`[name="${name}"]`, ui)) {
+      el.onchange = () => {
+        if (el.checked) autosave(el, setter);
+      };
+    }
+    return () => {
+      $(`[name="${name}"][value="${getter()}"]`, ui).checked = true;
     };
+  }
+
+  function save() {
+    pendingSetters.forEach((fn, el) => fn(el.value));
+    helpPopup.div.classList.remove('dirty');
+    elSave.disabled = true;
+  }
+
+  function textToList(text) {
+    return text.split(/\n/).map(s => s.trim()).filter(Boolean);
   }
 }
