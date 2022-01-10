openstyles/stylus

commit ba9d904ccc96cfc6b97356562fd96c38f20da058

don't rebuild editor DOM on save, fixes #1380

edit/sections-editor.js
@@ -108,9 +108,8 @@ function SectionsEditor() {
         return;
       }
       newStyle = await API.styles.editSave(newStyle);
-      destroyRemovedSections();
       sessionStore.justEditedStyleId = newStyle.id;
-      editor.replaceStyle(newStyle, false);
+      dirty.clear();
     },
 
     scrollToEditor(cm) {
@@ -451,18 +450,6 @@ function SectionsEditor() {
       }
     }
     return true;
-  }
-
-  function destroyRemovedSections() {
-    for (let i = 0; i < sections.length;) {
-      if (!sections[i].removed) {
-        i++;
-        continue;
-      }
-      sections[i].destroy();
-      sections[i].el.remove();
-      sections.splice(i, 1);
-    }
   }
 
   function updateMeta() {

commit 68dc584749c34742aae0ff37da05989388df264c

deduplicate editor.useSavedStyle
...and call it when saving in sectioned editor (regressed in ba9d904c)

edit/edit.js
@@ -266,6 +266,16 @@ window.on('beforeunload', e => {
         el.classList.add(cls);
       }
     },
+
+    useSavedStyle(newStyle) {
+      if (style.id !== newStyle.id) {
+        history.replaceState({}, '', `?id=${newStyle.id}`);
+      }
+      sessionStore.justEditedStyleId = newStyle.id;
+      Object.assign(style, newStyle);
+      editor.updateClass();
+      editor.updateMeta();
+    },
   });
 })();
 

edit/sections-editor.js
@@ -1,7 +1,7 @@
 /* global $ $$ $create $remove messageBoxProxy */// dom.js
 /* global API */// msg.js
 /* global CodeMirror */
-/* global FIREFOX RX_META debounce ignoreChromeError sessionStore */// toolbox.js
+/* global FIREFOX RX_META debounce ignoreChromeError */// toolbox.js
 /* global MozDocMapper clipString helpPopup rerouteHotkeys showCodeMirrorPopup */// util.js
 /* global createSection */// sections-editor-section.js
 /* global editor */
@@ -96,13 +96,7 @@ function SectionsEditor() {
       if (!sameCode) {
         await initSections(newStyle.sections, {replace: true});
       }
-      Object.assign(style, newStyle);
-      editor.updateClass();
-      updateMeta();
-      // Go from new style URL to edit style URL
-      if (style.id && !/[&?]id=/.test(location.search)) {
-        history.replaceState({}, document.title, `${location.pathname}?id=${style.id}`);
-      }
+      editor.useSavedStyle(newStyle);
       updateLivePreview();
     },
 
@@ -112,8 +106,8 @@ function SectionsEditor() {
         return;
       }
       newStyle = await API.styles.editSave(newStyle);
-      sessionStore.justEditedStyleId = newStyle.id;
       dirty.clear();
+      editor.useSavedStyle(newStyle);
     },
 
     scrollToEditor(cm) {

edit/source-editor.js
@@ -4,7 +4,7 @@
 /* global MozDocMapper */// util.js
 /* global MozSectionFinder */
 /* global MozSectionWidget */
-/* global RX_META debounce sessionStore */// toolbox.js
+/* global RX_META debounce */// toolbox.js
 /* global chromeSync */// storage-util.js
 /* global cmFactory */
 /* global editor */
@@ -214,14 +214,14 @@ function SourceEditor() {
     if (sameCode) {
       savedGeneration = cm.changeGeneration();
       dirty.clear('sourceGeneration');
-      updateEnvironment();
+      editor.useSavedStyle(newStyle);
       dirty.clear('enabled');
       updateLivePreview();
       return;
     }
 
     if (await messageBoxProxy.confirm(t('styleUpdateDiscardChanges'))) {
-      updateEnvironment();
+      editor.useSavedStyle(newStyle);
       if (!sameCode) {
         const cursor = cm.getCursor();
         cm.setValue(style.sourceCode);
@@ -233,16 +233,6 @@ function SourceEditor() {
         updateLivePreview();
       }
       dirty.clear();
-    }
-
-    function updateEnvironment() {
-      if (style.id !== newStyle.id) {
-        history.replaceState({}, '', `?id=${newStyle.id}`);
-      }
-      sessionStore.justEditedStyleId = newStyle.id;
-      Object.assign(style, newStyle);
-      editor.updateClass();
-      updateMeta();
     }
   }
 