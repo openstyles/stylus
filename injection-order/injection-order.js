ecbf397a78a7d3377f31a21e9ca890fd858c6493
tophf <tophf@gmx.com>
2022-01-13 13:30:16 +0300

    remove unused stuff, use dom.js, polish css

--- injection-order/injection-order.js
+++ injection-order/injection-order.js
@@ -1,42 +1,33 @@
+/* global $ $create */// dom.js
+/* global API */// msg.js
 /* global DraggableList */
 /* global prefs */
-/* global API */
 'use strict';
 
 (async () => {
-  const list = (await getOrderedStyles()).map(createLi);
-  const ol = document.querySelector('#style-list');
+  const list = (await getOrderedStyles()).map(style => ({
+    el: $create('li', [$create('span.dragger'), style.name]),
+    style,
+  }));
+  const ol = $('#style-list');
   let maxTranslateY;
   ol.append(...list.map(l => l.el));
-  ol.addEventListener('d:dragstart', e => {
+  ol.on('d:dragstart', e => {
     e.detail.origin.dataTransfer.setDragImage(new Image(), 0, 0);
     maxTranslateY = ol.scrollHeight - e.detail.dragTarget.offsetHeight - e.detail.dragTarget.offsetTop;
   });
-  ol.addEventListener('d:dragmove', e => {
+  ol.on('d:dragmove', e => {
     e.detail.origin.dataTransfer.dropEffect = 'move';
     const y = Math.min(e.detail.currentPos.y - e.detail.startPos.y, maxTranslateY);
     e.detail.dragTarget.style.transform = `translateY(${y}px)`;
   });
-  ol.addEventListener('d:dragend', e => {
+  ol.on('d:dragend', e => {
     const [item] = list.splice(e.detail.originalIndex, 1);
     list.splice(e.detail.spliceIndex, 0, item);
     ol.insertBefore(e.detail.dragTarget, e.detail.insertBefore);
     prefs.set('injectionOrder', list.map(l => l.style._id));
   });
   new DraggableList(ol, {scrollContainer: ol});
-  document.querySelector('#main').classList.add('ready');
-
-  document.querySelector('.closer').addEventListener('click', () => {
-    parent.dispatchEvent(new Event('closeOptions'));
-  });
-
-  function createLi(style) {
-    const el = document.createElement('li');
-    const dragger = document.createElement('span');
-    dragger.className = 'dragger';
-    el.append(dragger, style.name);
-    return {el, style};
-  }
 
   async function getOrderedStyles() {
     const [styles] = await Promise.all([
