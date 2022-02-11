openstyles/stylus

commit cc7eba979eb9ad0dabed8a7b654b50db085a757c

save-as-template button in editor (#1385)
+ keep i18n attributes to use them as CSS selectors
+ reduce flicker when creating a new style
+ split button

js/localization.js
@@ -73,7 +73,6 @@ Object.assign(t, {
         if (toInsert) {
           node.insertBefore(toInsert, before || null);
         }
-        node.removeAttribute(name);
       }
     }
   },

edit.html
@@ -271,13 +271,16 @@
         </div>
       </section>
       <section id="actions">
-        <div>
-          <button id="save-button" i18n-text="styleSaveLabel" data-hotkey-tooltip="save" disabled></button>
+        <div class="buttons">
+          <div class="split-btn">
+            <button id="save-button" i18n-text="styleSaveLabel" data-hotkey-tooltip="save" disabled></button
+            ><button class="split-btn-pedal usercss-only" i18n-menu-tpl="saveAsTemplate"></button>
+          </div>
           <button id="beautify" i18n-text="styleBeautify"></button>
           <button id="style-settings-btn" i18n-text="settings"></button>
           <button id="cancel-button" i18n-title="styleCancelEditLabel">↩</button>
         </div>
-        <div id="mozilla-format-buttons" class="sectioned-only">
+        <div id="mozilla-format-buttons" class="buttons sectioned-only">
           <button id="from-mozilla" i18n-text="importLabel"></button>
           <button id="to-mozilla" i18n-text="exportLabel"></button>
           <a id="to-mozilla-help" class="svg-inline-wrapper" tabindex="0"

edit/edit.css
@@ -260,23 +260,13 @@ input:invalid {
   margin-top: .5rem;
 }
 
-#actions > * {
+#actions .buttons {
   display: inline-flex;
-  flex-wrap: wrap;
-}
-
-#mozilla-format-buttons {
-  display: flex;
   flex-wrap: wrap;
   align-items: center;
 }
 
-#actions > div > a {
-  height: min-content;
-}
-
-#actions button,
-#actions > div > a {
+#actions .buttons > * {
   margin: 0 .2rem .5rem 0;
 }
 
@@ -1063,13 +1053,6 @@ body.linter-disabled .hidden-unless-compact {
   #lint > .lint-scroll-container {
     margin-left: 1rem;
     padding: .25rem 0 .5rem;
-  }
-  #actions {
-    display: flex;
-    flex-wrap: wrap;
-    white-space: nowrap;
-    margin: 0;
-    box-sizing: border-box;
   }
   #header input[type="checkbox"] {
     vertical-align: middle;

edit/edit.js
@@ -4,6 +4,7 @@
 /* global SectionsEditor */
 /* global SourceEditor */
 /* global baseInit */
+/* global chromeSync */// storage-util.js
 /* global clipString createHotkeyInput helpPopup */// util.js
 /* global closeCurrentTab deepEqual sessionStore tryJSONparse */// toolbox.js
 /* global cmFactory */
@@ -16,7 +17,10 @@
 //#region init
 
 baseInit.ready.then(async () => {
-  await waitForSheet();
+  [editor.template] = await Promise.all([
+    editor.isUsercss && !editor.style.id && chromeSync.getLZValue(chromeSync.LZ_KEY.usercssTemplate),
+    waitForSheet(),
+  ]);
   (editor.isUsercss ? SourceEditor : SectionsEditor)();
   await editor.ready;
   editor.ready = true;

edit/source-editor.js
@@ -17,20 +17,19 @@
 function SourceEditor() {
   const {style, /** @type DirtyReporter */dirty} = editor;
   let savedGeneration;
-  let placeholderName = '';
   let prevMode = NaN;
 
   $$remove('.sectioned-only');
   $('#header').on('wheel', headerOnScroll);
   $('#sections').textContent = '';
   $('#sections').appendChild($create('.single-editor'));
-
-  if (!style.id) setupNewStyle(style);
+  $('#save-button').onauxclick = e => e.detail === 'tpl' && saveTemplate();
 
   const cm = cmFactory.create($('.single-editor'));
   const sectionFinder = MozSectionFinder(cm);
   const sectionWidget = MozSectionWidget(cm, sectionFinder);
   editor.livePreview.init(preprocess);
+  if (!style.id) setupNewStyle();
   createMetaCompiler(meta => {
     style.usercssData = meta;
     style.name = meta.name;
@@ -75,13 +74,7 @@ function SourceEditor() {
         }
         showLog(res);
       } catch (err) {
-        const i = err.index;
-        const isNameEmpty = i > 0 &&
-          err.code === 'missingValue' &&
-          sourceCode.slice(sourceCode.lastIndexOf('\n', i - 1), i).trim().endsWith('@name');
-        return isNameEmpty
-          ? saveTemplate(sourceCode)
-          : showSaveError(err);
+        showSaveError(err);
       }
     },
     scrollToEditor: () => {},
@@ -160,7 +153,7 @@ function SourceEditor() {
     return name;
   }
 
-  async function setupNewStyle(style) {
+  function setupNewStyle() {
     style.sections[0].code = ' '.repeat(prefs.get('editor.tabSize')) +
       `/* ${t('usercssReplaceTemplateSectionBody')} */`;
     let section = MozDocMapper.styleToCss(style);
@@ -177,17 +170,11 @@ function SourceEditor() {
       @author         Me
       ==/UserStyle== */
     `.replace(/^\s+/gm, '');
-
-    dirty.clear('sourceGeneration');
-    style.sourceCode = '';
-
-    placeholderName = `${style.name || t('usercssReplaceTemplateName')} - ${new Date().toLocaleString()}`;
-    let code = await chromeSync.getLZValue(chromeSync.LZ_KEY.usercssTemplate);
-    code = code || DEFAULT_CODE;
-    code = code.replace(/@name(\s*)(?=[\r\n])/, (str, space) =>
-      `${str}${space ? '' : ' '}${placeholderName}`);
+    style.name = [style.name, new Date().toLocaleString()].filter(Boolean).join(' - ');
     // strip the last dummy section if any, add an empty line followed by the section
-    style.sourceCode = code.replace(/\s*@-moz-document[^{]*{[^}]*}\s*$|\s+$/g, '') + '\n\n' + section;
+    style.sourceCode = (editor.template || DEFAULT_CODE)
+      .replace(/(@name)(?:([\t\x20]+).*|\n)/, (_, k, space) => `${k}${space || ' '}${style.name}`)
+      .replace(/\s*@-moz-document[^{]*{[^}]*}\s*$|\s+$/g, '') + '\n\n' + section;
     cm.startOperation();
     cm.setValue(style.sourceCode);
     cm.clearHistory();
@@ -199,9 +186,7 @@ function SourceEditor() {
 
   function updateMeta() {
     const name = style.customName || style.name;
-    if (name !== placeholderName) {
-      $('#name').value = name;
-    }
+    $('#name').value = name;
     $('#enabled').checked = style.enabled;
     $('#url').href = style.url;
     editor.updateName();
@@ -236,9 +221,10 @@ function SourceEditor() {
     }
   }
 
-  async function saveTemplate(code) {
+  async function saveTemplate() {
     if (await messageBoxProxy.confirm(t('usercssReplaceTemplateConfirmation'))) {
       const key = chromeSync.LZ_KEY.usercssTemplate;
+      const code = cm.getValue();
       await chromeSync.setLZValue(key, code);
       if (await chromeSync.getLZValue(key) !== code) {
         messageBoxProxy.alert(t('syncStorageErrorSaving'));

global.css
@@ -33,6 +33,7 @@ button {
   border: 1px solid hsl(0, 0%, 62%);
   font: inherit;
   font-size: 13px;
+  line-height: 1.2;
   color: #000;
   background-color: hsl(0, 0%, 100%);
   background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAAeCAYAAADtlXTHAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4QwGBBwIHvKt6QAAAB1pVFh0Q29tbWVudAAAAAAAQ3JlYXRlZCB3aXRoIEdJTVBkLmUHAAAAL0lEQVQI12NoaGgQZ2JgYGBkYmBgYGZiYGBggrMY4VxsYsyoskQQCB2MWAxAMhkADVECDhlW9CoAAAAASUVORK5CYII=');
@@ -306,6 +307,39 @@ body.resizing-v > * {
   user-select: none;
 }
 /* header resizer - end */
+
+.split-btn {
+  position: relative;
+}
+.split-btn-pedal {
+  margin-left: -1px !important;
+  padding-left: .2em !important;
+  padding-right: .2em !important;
+}
+.split-btn-pedal::after {
+  content: '\25BC'; /* down triangle */
+  font-size: 90%;
+}
+.split-btn-pedal.active {
+  box-shadow: inset 0 0 100px rgba(0, 0, 0, .2);
+}
+.split-btn-menu {
+  background: #fff;
+  position: absolute;
+  box-shadow: 2px 3px 7px rgba(0, 0, 0, .5);
+  border: 1px solid hsl(180deg, 50%, 50%);
+  white-space: nowrap;
+  cursor: pointer;
+  padding: .25em 0;
+}
+.split-btn-menu > * {
+  padding: .5em 1em;
+  display: block;
+}
+.split-btn-menu > :hover {
+  background-color: hsla(180deg, 50%, 50%, .25);
+  color: #000;
+}
 
 @supports (-moz-appearance: none) {
   .moz-appearance-bug .svg-icon.checked,

js/dom-on-load.js
@@ -1,4 +1,4 @@
-/* global $ $$ focusAccessibility getEventKeyName */// dom.js
+/* global $$ $ $create focusAccessibility getEventKeyName moveFocus */// dom.js
 /* global debounce */// toolbox.js
 /* global t */// localization.js
 'use strict';
@@ -6,13 +6,14 @@
 /** DOM housekeeping after a page finished loading */
 
 (() => {
+  const SPLIT_BTN_MENU = '.split-btn-menu';
   splitLongTooltips();
   addTooltipsToEllipsized();
   window.on('mousedown', suppressFocusRingOnClick, {passive: true});
   window.on('keydown', keepFocusRingOnTabbing, {passive: true});
   window.on('keypress', clickDummyLinkOnEnter);
   window.on('wheel', changeFocusedInputOnWheel, {capture: true, passive: false});
-  window.on('click', showTooltipNote);
+  window.on('click', e => splitMenu(e) || showTooltipNote(e));
   window.on('resize', () => debounce(addTooltipsToEllipsized, 100));
   // Removing transition-suppressor rule
   const {sheet} = $('link[href$="global.css"]');
@@ -78,9 +79,36 @@
         let el = document.activeElement;
         if (el) {
           el = el.closest('[data-focused-via-click]');
-          if (el) delete el.dataset.focusedViaClick;
+          focusAccessibility.toggle(el, false);
         }
       });
+    }
+  }
+
+  function splitMenu(event) {
+    const prevMenu = $(SPLIT_BTN_MENU);
+    const prevPedal = (prevMenu || {}).previousElementSibling;
+    const pedal = event.target.closest('.split-btn-pedal');
+    const entry = prevMenu && event.target.closest(SPLIT_BTN_MENU + '>*');
+    if (prevMenu) prevMenu.remove();
+    if (prevPedal) prevPedal.classList.remove('active');
+    if (pedal && pedal !== prevPedal) {
+      const menu = $create(SPLIT_BTN_MENU,
+        Array.from(pedal.attributes, ({name, value}) =>
+          name.startsWith('menu-') &&
+          $create('a', {tabIndex: 0, __cmd: name.split('-').pop()}, value)
+        ));
+      menu.on('focusout', e => e.target === menu && splitMenu(e));
+      pedal.classList.toggle('active');
+      pedal.after(menu);
+      moveFocus(menu, 0);
+      focusAccessibility.toggle(menu.firstChild, focusAccessibility.get(pedal));
+    }
+    if (entry) {
+      prevPedal.previousElementSibling.dispatchEvent(new CustomEvent('auxclick', {
+        detail: entry.__cmd,
+        bubbles: true,
+      }));
     }
   }
 
@@ -88,9 +116,7 @@
     const el = focusAccessibility.closest(target);
     if (el) {
       focusAccessibility.lastFocusedViaClick = true;
-      if (el.dataset.focusedViaClick === undefined) {
-        el.dataset.focusedViaClick = '';
-      }
+      focusAccessibility.toggle(el, true);
     }
   }
 

js/dom.js
@@ -31,6 +31,8 @@ Object.assign(EventTarget.prototype, {
 const focusAccessibility = {
   // last event's focusedViaClick
   lastFocusedViaClick: false,
+  get: el => el && el.dataset.focusedViaClick != null,
+  toggle: (el, state) => el && toggleDataset(el, 'focusedViaClick', state),
   // to avoid a full layout recalc due to changes on body/root
   // we modify the closest focusable element (like input or button or anything with tabindex=0)
   closest(el) {

commit f9a2b9de3512c5f4d1d34bae85a3f7bad92dd6ef

fix/simplify i18n in templates
fix: double-translation, regressed in cc7eba9

js/localization.js
@@ -13,6 +13,7 @@ Object.assign(t, {
   template: {},
   parser: new DOMParser(),
   ALLOWED_TAGS: ['a', 'b', 'code', 'i', 'sub', 'sup', 'wbr'],
+  PREFIX: 'i18n-',
   RX_WORD_BREAK: new RegExp([
     '(',
     /[\d\w\u007B-\uFFFF]{10}/,
@@ -33,10 +34,12 @@ Object.assign(t, {
   },
 
   NodeList(nodes) {
-    const PREFIX = 'i18n-';
+    if (nodes instanceof Node) {
+      nodes = [nodes, ...nodes.getElementsByTagName('*')];
+    }
     for (let n = nodes.length; --n >= 0;) {
       const node = nodes[n];
-      if (node.nodeType !== Node.ELEMENT_NODE) {
+      if (!node.localName) {
         continue;
       }
       if (node.localName === 'template') {
@@ -46,10 +49,10 @@ Object.assign(t, {
       for (let a = node.attributes.length; --a >= 0;) {
         const attr = node.attributes[a];
         const name = attr.nodeName;
-        if (!name.startsWith(PREFIX)) {
+        if (!name.startsWith(t.PREFIX)) {
           continue;
         }
-        const type = name.substr(PREFIX.length);
+        const type = name.substr(t.PREFIX.length);
         const value = t(attr.value);
         let toInsert, before;
         switch (type) {
@@ -73,6 +76,7 @@ Object.assign(t, {
         if (toInsert) {
           node.insertBefore(toInsert, before || null);
         }
+        node.removeAttribute(name);
       }
     }
   },
@@ -84,20 +88,16 @@ Object.assign(t, {
   },
 
   createTemplate(node) {
-    const elements = node.content.querySelectorAll('*');
-    t.NodeList(elements);
-    t.template[node.dataset.id] = elements[0];
+    const el = node.content.firstElementChild.cloneNode(true);
+    t.NodeList(el);
+    t.template[node.dataset.id] = el;
     // compress inter-tag whitespace to reduce number of DOM nodes by 25%
-    const walker = document.createTreeWalker(elements[0], NodeFilter.SHOW_TEXT);
-    const toRemove = [];
-    while (walker.nextNode()) {
-      const textNode = walker.currentNode;
-      if (!/[\xA0\S]/.test(textNode.nodeValue)) { // allow \xA0 to keep &nbsp;
-        toRemove.push(textNode);
+    const walker = document.createTreeWalker(el, NodeFilter.SHOW_TEXT);
+    for (let n; (n = walker.nextNode());) {
+      if (!/[\xA0\S]/.test(n.textContent)) { // allow \xA0 to keep &nbsp;
+        n.remove();
       }
     }
-    t.stopObserver();
-    toRemove.forEach(el => el.remove());
   },
 
   createText(str) {
@@ -109,7 +109,7 @@ Object.assign(t, {
     if (!trusted) {
       t.sanitizeHtml(root);
     } else if (str.includes('i18n-')) {
-      t.NodeList(root.getElementsByTagName('*'));
+      t.NodeList(root);
     }
     const bin = document.createDocumentFragment();
     while (root.firstChild) {
@@ -122,7 +122,7 @@ Object.assign(t, {
     let el = t.template[name];
     if (!el) {
       el = (await download(url, {responseType: 'document'})).body.firstElementChild;
-      t.NodeList(el.getElementsByTagName('*'));
+      t.NodeList(el);
       t.template[name] = el;
     }
     return el;
@@ -201,7 +201,7 @@ Object.assign(t, {
     t.stopObserver();
   }, {once: true});
 
-  t.NodeList(document.getElementsByTagName('*'));
+  t.NodeList(document);
   start();
 
   function process(mutations) {