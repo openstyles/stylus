openstyles/stylus

commit 81a5acfda37d9e2ed66dff4076ac645669b1b212

.user.css in virtual style urls
benefits:
* styles are listed directly under `Stylus` in devtools source file tree
* easy to differentiate from site's css

background/style-via-webrequest.js
@@ -51,7 +51,7 @@
     }
     if (CHROME) {
       chrome.webRequest.onBeforeRequest.addListener(openNamedStyle, {
-        urls: [URLS.ownOrigin + 'virtual/*.css'],
+        urls: [URLS.ownOrigin + '*.user.css'],
         types: ['main_frame'],
       }, ['blocking']);
     }

content/style-injector.js
@@ -150,7 +150,7 @@ window.StyleInjector = window.INJECTED === 1 ? window.StyleInjector : ({
     if (exposeStyleName && name) {
       el.dataset.name = name;
       name = encodeURIComponent(name.replace(/[#%/@:']/g, toSafeChar));
-      code += `\n/*# sourceURL=${chrome.runtime.getURL('')}virtual/${name}.css#${id} */`;
+      code += `\n/*# sourceURL=${chrome.runtime.getURL(name)}.user.css#${id} */`;
     }
     el.type = 'text/css';
     // SVG className is not a string, but an instance of SVGAnimatedString

commit 225a2cec31827f788bed8ef274e55276e6403e2c

unbork installer, regressed in 81a5acfd

background/style-via-webrequest.js
@@ -154,8 +154,10 @@
 
   /** @param {chrome.webRequest.WebRequestBodyDetails} req */
   function openNamedStyle(req) {
-    chrome.tabs.update(req.tabId, {url: 'edit.html?id=' + req.url.split('#')[1]});
-    return {cancel: true};
+    if (!req.url.includes('?')) { // skipping our usercss installer
+      chrome.tabs.update(req.tabId, {url: 'edit.html?id=' + req.url.split('#')[1]});
+      return {cancel: true};
+    }
   }
 
   function req2key(req) {

content/style-injector.js
@@ -158,7 +158,7 @@ window.StyleInjector = window.INJECTED === 1 ? window.StyleInjector : ({
   function setTextAndName(el, {id, code = '', name}) {
     if (exposeStyleName && name) {
       el.dataset.name = name;
-      name = encodeURIComponent(name.replace(/[#%/@:']/g, toSafeChar));
+      name = encodeURIComponent(name.replace(/[?#%/@:']/g, toSafeChar));
       code += `\n/*# sourceURL=${chrome.runtime.getURL(name)}.user.css#${id} */`;
     }
     el.textContent = code;