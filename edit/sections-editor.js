openstyles/stylus

commit e68e14d0e8a19a8c2609bf090c04ef83ce6f546b

fix fitToAvailableSpace + reduce section padding

edit/sections-editor.js
@@ -172,8 +172,9 @@ function SectionsEditor() {
     }
   }
 
-  function fitToAvailableSpace() {
-    const lastSectionBottom = sections[sections.length - 1].el.getBoundingClientRect().bottom;
+  function fitToAvailableSpace(entries, observer) {
+    observer.disconnect();
+    const lastSectionBottom = (/** @type {IntersectionObserverEntry}*/entries[0]).boundingClientRect.bottom;
     const delta = Math.floor((window.innerHeight - lastSectionBottom) / sections.length);
     if (delta > 1) {
       sections.forEach(({cm}) => {
@@ -547,7 +548,7 @@ function SectionsEditor() {
       if (iSec === focusOn) setTimeout(editor.jumpToEditor, 0, iSec);
     }
     if (!si || si.cms.every(cm => !cm.height)) {
-      requestAnimationFrame(fitToAvailableSpace);
+      new IntersectionObserver(fitToAvailableSpace).observe(container);
     }
     container.style.removeProperty('height');
     setGlobalProgress();

edit/edit.css
@@ -334,7 +334,7 @@ i,
   margin-left: var(--padL);
 }
 .section:nth-child(n + 2) {
-  border-top: 4px double var(--c80);
+  border-top: 1px dotted var(--c80);
 }
 .add-section:after {
   content: attr(short-text);
@@ -972,9 +972,6 @@ i,
 
 /************ reponsive layouts ************/
 @media(max-width: 850px) {
-  .sectioned body {
-    height: 100%;
-  }
   .usercss body {
     display: flex;
     flex-direction: column;
@@ -1105,8 +1102,7 @@ i,
     padding: 0;
   }
   .section {
-    margin: 0;
-    padding: var(--pad) var(--pad05);
+    padding: var(--pad05);
   }
   .single-editor {
     overflow: hidden;

commit 246ae6c45a64fd06a145199a0c8db129a5286264

faster auto-sizing, regressed by e68e14d0

edit/sections-editor.js
@@ -173,9 +173,8 @@ function SectionsEditor() {
     }
   }
 
-  function fitToAvailableSpace(entries, observer) {
-    observer.disconnect();
-    const lastSectionBottom = (/** @type {IntersectionObserverEntry}*/entries[0]).boundingClientRect.bottom;
+  function fitToAvailableSpace() {
+    const lastSectionBottom = container.getBoundingClientRect().bottom;
     const delta = Math.floor((window.innerHeight - lastSectionBottom) / sections.length);
     if (delta > 1) {
       sections.forEach(({cm}) => {
@@ -530,7 +529,7 @@ function SectionsEditor() {
       if (iSec === focusOn) setTimeout(editor.jumpToEditor, 0, iSec);
     }
     if (!si || si.cms.every(cm => !cm.height)) {
-      new IntersectionObserver(fitToAvailableSpace).observe(container);
+      requestAnimationFrame(fitToAvailableSpace); // avoids FOUC, unlike IntersectionObserver
     }
     container.style.removeProperty('height');
     setGlobalProgress();