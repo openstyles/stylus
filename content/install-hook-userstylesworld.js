bd22c606b52316860a34cf208b478282ac71ca1f
tophf <extophf@gmail.com>
2023-12-18 19:34:19 +0300

    restore on-demand USW injection
    ...just in case AMO/CWS still incorrectly disable the extension after a new content script entry is added

--- content/install-hook-userstylesworld.js
+++ content/install-hook-userstylesworld.js
@@ -1,43 +1,45 @@
 /* global API */// msg.js
 'use strict';
 
-window.USW !== 1 && (() => { // eslint-disable-line no-unused-expressions
+if (window.USW !== 1) {
   window.USW = 1; // avoiding re-injection
+  let filledInfo;
   const ORIGIN = location.origin;
-  const HANDLERS = Object.assign(Object.create(null), {
+  const send = (type, data) => postMessage({type, data}, ORIGIN);
 
-    async 'usw-ready'() {
-      send({type: 'usw-remove-stylus-button'});
-      if (location.pathname === '/api/oauth/style/new') {
-        const styleId = Number(new URLSearchParams(location.search).get('vendor_data'));
-        const data = await API.data.get('usw' + styleId);
-        send({type: 'usw-fill-new-style', data});
-      }
-    },
+  const onReady = async function (data) {
+    if (data) {
+      send('usw-remove-stylus-button');
+    }
+    if (location.pathname === '/api/oauth/style/new') {
+      filledInfo = true;
+      const styleId = +new URLSearchParams(location.search).get('vendor_data');
+      const data = await API.data.get('usw' + styleId);
+      send('usw-fill-new-style', data);
+    }
+  };
 
+  const HANDLERS = {
+    __proto__: null,
+    'usw-ready': onReady,
     async 'usw-style-info-request'(data) {
       switch (data.requestType) {
         case 'installed': {
           const updateUrl = `${ORIGIN}/api/style/${data.styleID}.user.css`;
           const style = await API.styles.find({updateUrl});
-          send({
-            type: 'usw-style-info-response',
-            data: {installed: Boolean(style), requestType: 'installed'},
-          });
+          data.installed = !!style;
+          send('usw-style-info-response', data);
           break;
         }
       }
     },
-  });
+  };
 
+  if (!filledInfo) onReady();
   addEventListener('message', ({data, origin}) => {
     if (chrome.runtime.id && data && origin === ORIGIN) {
       const fn = HANDLERS[data.type];
       if (fn) fn(data);
     }
   });
-
-  function send(msg) {
-    window.postMessage(msg, ORIGIN);
-  }
-})();
+}
