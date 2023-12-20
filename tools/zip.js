d3a77285a0d0d845bf4faa7306c362974dcc6f50
tophf <extophf@gmail.com>
2023-12-20 23:47:12 +0300

    use :is() + fix zip

--- tools/zip.js
+++ tools/zip.js
@@ -24,6 +24,7 @@ const sFirefox = 'firefox';
     'tools',
   ];
   const mjStr = fs.readFileSync(MANIFEST, 'utf8');
+  const cssFiles = [];
   const jobs = [];
   // https://github.com/Stuk/jszip/issues/369
   const tzBug = new Date().getTimezoneOffset() * 60000;
@@ -31,12 +32,20 @@ const sFirefox = 'firefox';
   // add all files except manifest.json
   let zip = new JSZip();
   for (const e of glob.sync(ADD, {ignore: SKIP, stats: true, onlyFiles: true})) {
-    zip.file(e.path, fs.readFileSync(e.path, 'utf8'), {date: new Date(e.stats.mtime - tzBug)});
+    const bytes = fs.readFileSync(e.path);
+    const date = new Date(e.stats.mtime - tzBug);
+    let text;
+    if (e.path.endsWith('.css') && (text = bytes.toString('utf8')).includes(':is(')) {
+      cssFiles.push([e.path, date, text]);
+    } else {
+      zip.file(e.path, bytes, {date});
+    }
   }
   let buf;
   // add a patched manifest.json for each zip reusing compressed data for all other files
   for (const suffix of [sChrome, sChromeBeta, sFirefox]) {
     if (buf) zip = await zip.loadAsync(buf);
+    if (suffix !== sChromeBeta) patchCss(zip, cssFiles, suffix);
     const mj = patchManifest(mjStr, suffix);
     const fileName = `stylus-${suffix}-${mj.version}.zip`;
     zip.file(MANIFEST, JSON.stringify(mj, null, 2));
@@ -49,6 +58,13 @@ const sFirefox = 'firefox';
   console.error(err);
   process.exit(1);
 });
+
+function patchCss(zip, files, suffix) {
+  const any = suffix === sFirefox ? ':-moz-any(' : ':-webkit-any(';
+  for (const [path, date, text] of files) {
+    zip.file(path, text.replaceAll(':is(', any), {date});
+  }
+}
 
 function patchManifest(str, suffix) {
   const mj = JSON.parse(str);
