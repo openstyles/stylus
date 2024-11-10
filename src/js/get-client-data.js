ad4e069c69d548fcad2b1cf563d05b02578f5d59
tophf <extophf@gmail.com>
2024-11-10 16:13:17 +0300

    fix clientData in MV3

--- src/js/get-client-data.js
+++ src/js/get-client-data.js
@@ -1,6 +1,6 @@
 import {isCssDarkScheme, makePropertyPopProxy} from './util';
 
-self[process.env.CLIENT_DATA] = makePropertyPopProxy();
+self[process.env.CLIENT_DATA] = makePropertyPopProxy({});
 document.write(`<script src="?clientData&${new URLSearchParams({
   dark: +isCssDarkScheme(),
   url: location,
