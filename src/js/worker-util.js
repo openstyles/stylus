342abcf5296c8c2bda5358f1b3f90afcf045cdbb
tophf <extophf@gmail.com>
2024-12-15 01:47:06 +0300

    reuse global url registry in importScriptsOnce

--- src/js/worker-util.js
+++ src/js/worker-util.js
@@ -1,7 +1,8 @@
 const loadedUrls = [];
+const KEY = 'importScriptsOnce';
 
-export const importScriptsOnce = (...urls) => {
-  // All files are in the same directory; not loading via an absolute path as it's bugged in Orion
+export const importScriptsOnce = __.ENTRY !== 'worker' ? global[KEY] : global[KEY] = (...urls) => {
+  // `urls` are actually path-less file names in the same directory
   urls = urls.filter(u => !loadedUrls.includes(u));
   if (urls.length) {
     loadedUrls.push(...urls);
