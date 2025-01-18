55958b1f62befe7bb57024b5f9c1c1a4065249dc
tophf <extophf@gmail.com>
2025-01-18 03:38:59 +0300

    skip untranslated locales + commit in js

--- tools/fix-transifex.js
+++ tools/fix-transifex.js
@@ -1,10 +1,14 @@
 'use strict';
 
+const childProcess = require('child_process');
 const fs = require('fs');
 const {SRC} = require('./util');
 
 const DIR = SRC + '_locales/';
 const RX_LNG_CODE = /^\w\w(_\w{2,3})?$/; // like `en` or `en_GB`
+
+const ARG = process.argv[2];
+const COMMIT_CMD = ARG === 'commit' && `git commit -m "update locales" ${DIR}`;
 
 const makeFileName = lng => `${DIR}${lng}/messages.json`;
 const readLngJson = lng => JSON.parse(
@@ -36,6 +40,10 @@ for (const /**@type Dirent*/ entry of fs.readdirSync(DIR, {withFileTypes: true})
   }
 }
 console.log(`${numFixed} files fixed out of ${numTotal}`);
+
+if (COMMIT_CMD) try {
+  childProcess.execSync(COMMIT_CMD, {stdio: 'inherit'});
+} catch {}
 
 function fixLngFile(lng) {
   let numUnknown = 0;
