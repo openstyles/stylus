b77d2917bfce2aafdde4a6fd7efdfeca8dd751aa
tophf <extophf@gmail.com>
2025-02-14 16:00:55 +0300

    fix #1924: use storage.session in mv2-bg

--- src/js/storage-util.js
+++ src/js/storage-util.js
@@ -8,7 +8,5 @@ const StorageExtras = {
 
 export const chromeLocal =
   /*@__PURE__*/Object.assign(browser.storage.local, StorageExtras);
-/** @type {typeof import('chrome-types').chrome.storage.session} */
-export const chromeSession = __.MV3 &&
-  /*@__PURE__*/Object.assign(chrome.storage.session, StorageExtras);
+export const chromeSession = chrome.storage.session;
 export const GET_KEYS = !!chromeLocal.getKeys;
