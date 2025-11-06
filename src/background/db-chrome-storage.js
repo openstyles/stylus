7172790af6591e7e68e81aec0b2921e2ddee2cbf
tophf <extophf@gmail.com>
2025-11-06 21:16:33 +0300

    clarify _init as _getDbMax

--- src/background/db-chrome-storage.js
+++ src/background/db-chrome-storage.js
@@ -4,7 +4,7 @@ import {chromeLocal, GET_KEYS} from '@/js/storage-util';
 export default class ChromeStorageDB {
 
   constructor(dbName, mirror) {
-    this._max = dbName === DB ? 0 : 1;
+    this._max = dbName === DB ? null : 1; // used only with DB
     this._mirror = mirror;
     this._prefix = dbName === DB ? 'style-' : `${dbName}-`;
   }
@@ -21,8 +21,7 @@ export default class ChromeStorageDB {
     const all = !GET_KEYS && await chromeLocal.get();
     const keys = GET_KEYS ? await chromeLocal.getKeys() : Object.keys(all);
     const res = [];
-    if (!this._max)
-      await this._init(keys);
+    this._max ??= await this._getDbMax(keys);
     for (const key of keys)
       if (key.startsWith(this._prefix))
         res.push(GET_KEYS ? key : all[key]);
@@ -32,7 +31,7 @@ export default class ChromeStorageDB {
   }
 
   async put(item, key) {
-    key ??= item.id ??= (!this._max && await this._init(), this._max++);
+    key ??= item.id ??= (this._max ??= await this._getDbMax(), this._max++);
     await chromeLocal.set({
       [this._prefix + key]: this._mirror && item[UCD]
         ? {...item, sections: undefined}
@@ -46,7 +45,7 @@ export default class ChromeStorageDB {
     const res = [];
     for (let i = 0; i < items.length; i++) {
       const item = items[i];
-      const id = keys ? keys[i] : item.id ??= (!this._max && await this._init(), this._max++);
+      const id = keys ? keys[i] : item.id ??= (this._max ??= await this._getDbMax(), this._max++);
       data[this._prefix + id] = this._mirror && item[UCD]
         ? {...item, sections: undefined}
         : item;
@@ -56,7 +55,7 @@ export default class ChromeStorageDB {
     return res;
   }
 
-  async _init(keys) {
+  async _getDbMax(keys) {
     let res = 1;
     let id;
     keys ??= GET_KEYS
@@ -65,6 +64,6 @@ export default class ChromeStorageDB {
     for (const key of keys)
       if (key.startsWith(this._prefix) && (id = +key.slice(this._prefix.length)) >= res)
         res = id + 1;
-    this._max = res;
+    return res;
   }
 }
