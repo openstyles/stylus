8bd79e6d652991ba3c78978c4720026ac3692536
tophf <tophff@gmail.com>
2025-11-28 23:31:20 +0300

    support /re/ syntax in site lists

--- src/background/option-sites.js
+++ src/background/option-sites.js
@@ -8,8 +8,8 @@ const OPT_IDS = [pExposeIframes, pPatchCsp, pStyleViaASS, pStyleViaXhr];
 /** Since space isn't allowed in values, it marks our regex logic so it can be restored
  * in one replace() call on the result instead of escaping each value individually */
 const ALL_RE = ' [-a-z ] +://';
-//               0:         1:*          2:- 3:scheme                   4:host             5:path
-const SITE_RE = /(?:^|\s)(?:(\*)(?=\s|$)|(-)?((?:(?:ht|f)tps?|\*):\/\/)?([-\w.*]+(?::\d+)?)(\/[^\s#]*)?)/gi;
+//               0:  1:*   2:- 3:scheme                   4:host             5:path
+const SITE_RE = /^(?:(\*)$|(-)?((?:(?:ht|f)tps?|\*):\/\/)?([-\w.*]+(?::\d+)?)(\/[^\s#]*)?)/i;
 /** @typedef {{on?: string|RegExp, off?: string|RegExp, str: string}} OptionSitesData */
 /** @type {{[key: string]: false | OptionSitesData}} */
 export const optionSites = {};
@@ -43,48 +43,73 @@ function update() {
       optionSites[id] = false;
       continue;
     }
-    const str = __values[id + kSites].trim().toLowerCase().split(/\s+/).sort().join('\n');
+    const arr = __values[id + kSites].trim().toLowerCase().split(/\s+/).sort();
+    const str = arr.join('\n');
     if (str === optionSites[id]?.str)
       continue;
     const data = optionSites[id] = /** @type {OptionSitesData} */ {};
-    for (let m, not, hasAll; (m = SITE_RE.exec(str));) {
+    const seen = new Set();
+    const regexps = {};
+    let hasAll;
+    for (let m of arr) {
+      const not = m.charCodeAt(0) === 45/* - */;
+      const re = m.charCodeAt(not) === 47/* / */ && m.charCodeAt(m.length - 1) === 47
+        && m.slice(1 + not, -1);
+      m = re ? [re] : SITE_RE.exec(str);
+      if (!m || seen.has(m[0]))
+        continue;
+      seen.add(m[0]);
       if (m[1]) {
         hasAll = data.on = true;
-      } else if ((not = m[2]) || !hasAll) {
-        (data[not ? 'off' : 'on'] ??= new Set()).add([
-          // API reports scheme and host in lowercase
-          (m[3] || ALL_RE).toLowerCase(),
-          m[4].replace('*.', ' ( ?: [ ^:/ ] + \\ . ) ?').toLowerCase() + (m[5] || '/*'),
-        ]);
-      }
-    }
-    for (const [k, val] of Object.entries(data)) {
-      if (val === true)
-        continue;
-      const res = [' ^']; // reserving first elem to modify it for `multiSchemes`
-      let cur, curScheme, multiSchemes;
-      for (const [scheme, hostPath] of val) {
-        if (!cur) { // first iteration
-          res.push(scheme);
-          curScheme = scheme;
-          cur = hostPath;
-        } else if (curScheme !== scheme) {
-          if (cur) res.push(cur, ' |', scheme); // just one site in previous run
-          else res.push(' ) |', scheme);
-          multiSchemes = true;
-          curScheme = scheme;
-          cur = hostPath;
-        } else if (cur) {
-          res.push(' ( ?:', cur, ' |', hostPath);
-          cur = null;
+      } else if (not || !hasAll) {
+        const type = not ? 'off' : 'on';
+        data[type] ??= [];
+        if (re) {
+          (regexps[type] ??= []).push(re);
         } else {
-          res.push(' |', hostPath);
+          data[type].push([
+            // API reports scheme and host in lowercase
+            (m[3] || ALL_RE).toLowerCase(),
+            m[4].replace('*.', ' ( ?: [ ^:/ ] + \\ . ) ?').toLowerCase() + (m[5] || '/*'),
+          ]);
         }
       }
-      cur = (cur || curScheme && ' )' || '') + (multiSchemes ? ' )' : '');
-      if (cur) res.push(cur);
-      if (multiSchemes) res[0] += ' ( ?:';
-      data[k] = tryRegExp(globAsRegExpStr(res.join('')).replace(/ \\/g, ''));
+    }
+    for (const [k, globs] of Object.entries(data)) {
+      if (globs === true)
+        continue;
+      let res;
+      if (globs.length) {
+        res = ['']; // reserving first elem to modify it for `multiSchemes`
+        let cur, curScheme, multiSchemes;
+        for (const [scheme, hostPath] of globs) {
+          if (!cur) { // first iteration
+            res.push(scheme);
+            curScheme = scheme;
+            cur = hostPath;
+          } else if (curScheme !== scheme) {
+            if (cur) res.push(cur, ' |', scheme); // just one site in previous run
+            else res.push(' ) |', scheme);
+            multiSchemes = true;
+            curScheme = scheme;
+            cur = hostPath;
+          } else if (cur) {
+            res.push(' ( ?:', cur, ' |', hostPath);
+            cur = null;
+          } else {
+            res.push(' |', hostPath);
+          }
+        }
+        cur = (cur || curScheme && ' )' || '') + (multiSchemes ? ' )' : '');
+        if (cur) res.push(cur);
+        res[0] = multiSchemes ? ' ^ ( ?:' : ' ^';
+        res = globAsRegExpStr(res.join('')).replace(/ \\/g, '');
+      }
+      // TODO: show the error in the options
+      data[k] = tryRegExp(
+        (res || '') +
+        (regexps[k] ? `${res ? '|' : ''}^(?:${regexps[k].join('|')})$` : '')
+      );
       data.str = str;
     }
   }
