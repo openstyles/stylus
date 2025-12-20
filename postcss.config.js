f2910acdaae416bf73f6860afd0415a4874895f0
tophf <tophff@gmail.com>
2025-12-20 15:06:25 +0300

    fix postcss warnings for nesting

--- postcss.config.js
+++ postcss.config.js
@@ -4,15 +4,21 @@ const {getBrowserlist} = require('./tools/util');
 
 module.exports = {
   plugins: [
+    'postcss-simple-vars',
+    'postcss-nested', // see 'nesting-rules' comment
     ['postcss-preset-env', {
       browsers: getBrowserlist(),
       features: {
         'clamp': false, // used intentionally with a fallback
+        'is-pseudo-class': {
+          specificityMatchingName: 'Z',
+        },
+        /** disabling the built-in postcss-nesting plugin because is uses :is() for correctness,
+         * but it requires postcss-is-pseudo-class plugin which emits warnings about our css,
+         * so we use a different postcss-nested plugin that seemingly works just fine. */
+        'nesting-rules': false,
         'prefers-color-scheme-query': false, // we manually handle it via cssRules
       },
     }],
-    'postcss-simple-vars',
-    'postcss-nested',
-    'autoprefixer',
   ],
 };
