openstyles/stylus

commit 527e1231317b80c5380db13f607fbbb9e34ee885

paste/import from a cloud-sync single json

src/manage/import-export.js
@@ -69,7 +69,7 @@ async function importFromFile(file) {
     }
     const text = await textPromise;
     el.remove();
-    if (/^\s*\[/.test(text)) {
+    if (/^\s*[[{]/.test(text)) {
       await importFromString(text);
       setTimeout(() => queue.styles.clear(), IMPORT_THROTTLE * 2);
     } else if (RX_META.test(text)) {
@@ -95,7 +95,9 @@ async function importFromFile(file) {
 }
 
 async function importFromString(jsonString) {
-  const json = JSON.parse(jsonString);
+  let json = JSON.parse(jsonString) || [];
+  if (json._rev === json.doc?._rev)
+    json = [json.doc];
   const oldStyles = Array.isArray(json) && json.length ? await API.styles.getAll() : [];
   const oldStylesSet = new Set(oldStyles.sort((a, b) =>
     (a = a.customName || a.name).toLowerCase() <

src/edit/codemirror-factory.js
@@ -2,6 +2,8 @@ import {CodeMirror, loadCmTheme, THEME_KEY} from '@/cm';
 import {getStyleAtPos} from '@/cm/util';
 import {kCodeMirror} from '@/js/consts';
 import * as prefs from '@/js/prefs';
+import {styleJSONseemsValid, styleToCss} from '@/js/sections-util';
+import {tryJSONparse} from '@/js/util';
 import editor from './editor';
 import {rerouteHotkeys} from './util';
 
@@ -80,10 +82,28 @@ const onCmOption = (cm, name) => {
     cm[cm.options[name] ? 'on' : 'off']('beforeChange', onCmBeforeChange);
   }
 };
+const maybeImportOnPaste = (cm, evt) => {
+  let v, ucText;
+  let text = evt.clipboardData.getData('text') || '';
+  if (!/^\s*{\s*"doc"\s*:\s*{\s*"/.test(text)
+  || !(v = tryJSONparse(text))
+  || !styleJSONseemsValid(v = v.doc)) {
+    v = null;
+  } else if (!(ucText = v.sourceCode)) {
+    text = styleToCss(v);
+  }
+  if (!editor.isUsercss) {
+    editor.importOnPaste(cm, evt, ucText || text, !ucText && v?.sections);
+  } else if (v) {
+    evt.preventDefault();
+    cm.setValue(ucText || text);
+  }
+};
 CodeMirror.defineInitHook(cm => {
   cm.on('focus', onCmFocus);
   cm.on('blur', onCmBlur);
   cm.on('optionChange', onCmOption);
+  cm.on('paste', maybeImportOnPaste);
   if (cm.options[kLineWrapping]) cm.on('beforeChange', onCmBeforeChange);
 });
 

src/edit/sections-editor.js
@@ -53,6 +53,7 @@ export default function SectionsEditor() {
     sections,
 
     closestVisible,
+    importOnPaste,
     updateLivePreview,
     updateMeta,
 
@@ -375,7 +376,7 @@ export default function SectionsEditor() {
     popup.codebox.execCommand('selectAll');
   }
 
-  function showMozillaFormatImport(text = '') {
+  function showMozillaFormatImport(text, newSections) {
     const popup = showCodeMirrorPopup(t('styleFromMozillaFormatPrompt'),
       $create('.buttons', [
         $create('button', {
@@ -390,21 +391,25 @@ export default function SectionsEditor() {
           title: 'Ctrl-Enter:\n' + t('importAppendTooltip'),
           onclick: doImport,
         }),
-      ]));
+      ]),
+      {
+        readOnly: !!text,
+      });
     const contents = popup._contents;
-    contents.insertBefore(popup.codebox.display.wrapper, contents.firstElementChild);
-    popup.codebox.focus();
-    popup.codebox.on('changes', cm => {
+    const cm = popup.codebox;
+    contents.insertBefore(cm.display.wrapper, contents.firstElementChild);
+    cm.focus();
+    cm.on('changes', () => {
       popup.classList.toggle('ready', !cm.isBlank());
       cm.markClean();
     });
     if (text) {
-      popup.codebox.setValue(text);
-      popup.codebox.clearHistory();
-      popup.codebox.markClean();
+      cm.setValue(text);
+      cm.clearHistory();
+      cm.markClean();
     }
     // overwrite default extraKeys as those are inapplicable in popup context
-    popup.codebox.options.extraKeys = {
+    cm.options.extraKeys = {
       'Ctrl-Enter': doImport,
       'Shift-Ctrl-Enter': () => doImport({replaceOldStyle: true}),
     };
@@ -412,14 +417,17 @@ export default function SectionsEditor() {
     async function doImport({replaceOldStyle = false}) {
       lockPageUI(true);
       try {
-        const code = popup.codebox.getValue().trim();
-        if (!code.match(RX_META)?.[0].includes('@preprocessor') ||
-            !await getPreprocessor(code) ||
+        text ||= cm.getValue().trim();
+        if (!text.match(RX_META)?.[0].includes('@preprocessor') ||
+            !await getPreprocessor(text) ||
             await messageBox.confirm(
               t('importPreprocessor'), 'pre-line',
               t('importPreprocessorTitle'))
         ) {
-          const {sections: newSections, errors} = await worker.parseMozFormat({code});
+          let errors;
+          if (!newSections) {
+            ({sections: newSections, errors} = await worker.parseMozFormat({code: text}));
+          }
           if (!newSections.length || errors.some(e => !e.recoverable)) {
             await Promise.reject(errors);
           }
@@ -444,10 +452,10 @@ export default function SectionsEditor() {
 
     function lockPageUI(locked) {
       $root.style.pointerEvents = locked ? 'none' : '';
-      if (popup.codebox) {
-        popup.classList.toggle('ready', locked ? false : !popup.codebox.isBlank());
-        popup.codebox.options.readOnly = locked;
-        popup.codebox.display.wrapper.style.opacity = locked ? '.5' : '';
+      if (popup.codebox === cm) {
+        popup.classList.toggle('ready', locked ? false : !cm.isBlank());
+        cm.options.readOnly = locked;
+        cm.display.wrapper.style.opacity = locked ? '.5' : '';
       }
     }
 
@@ -641,17 +649,15 @@ export default function SectionsEditor() {
 
   /** @param {EditorSection} section */
   function registerEvents(section) {
-    const {el, cm} = section;
+    const {el} = section;
     el.$('.remove-section').onclick = () => removeSection(section);
     el.$('.add-section').onclick = () => insertSectionAfter(undefined, section);
     el.$('.clone-section').onclick = () => insertSectionAfter(section.getModel(), section);
     el.$('.move-section-up').onclick = () => moveSectionUp(section);
     el.$('.move-section-down').onclick = () => moveSectionDown(section);
-    cm.on('paste', maybeImportOnPaste);
   }
 
-  function maybeImportOnPaste(cm, event) {
-    const text = event.clipboardData.getData('text') || '';
+  function importOnPaste(cm, event, text) {
     if (/@-moz-document/i.test(text) &&
         /@-moz-document\s+(url|url-prefix|domain|regexp)\(/i
           .test(text.replace(/\/\*([^*]+|\*(?!\/))*(\*\/|$)/g, ''))

src/edit/util.js
@@ -16,14 +16,20 @@ export const helpPopup = {
    * @param {string|true} title - plain text or `true` to use `body` instead of .title and .contents
    * @param {AppendableElementGuts} body - Node, html or plain text
    * @param {WritableElementProps} [props] - DOM props for the popup element
-   * @param {Object} [dataset] - dataset props for the popup element
-   * @returns {HTMLElement & {onClose: Set<function>}} the popup
+   * @param {string} [id]
+   * @returns {HelpPopupElement} the popup
    */
   show(title = '', body, props, id = title) {
     const div = $create(helpPopup.SEL, props);
     const old = id && $(`${helpPopup.SEL}[data-id="${CSS.escape(id)}"] > .i-close`);
     if (old) old.click();
     div.dataset.id = id;
+    /** @typedef {HTMLElement & {
+      _close: HTMLElement,
+      _title: HTMLElement,
+      _contents: HTMLElement
+      onClose: Set<function>,
+    }} HelpPopupElement */
     div.append(
       div._close = $create('i.i-close', {onclick: helpPopup.close}),
       div._title = $create('.title', title),
@@ -176,6 +182,12 @@ export function createHotkeyInput(prefId, {buttons = true, onDone}) {
   }
 }
 
+/**
+ * @param {string | true} title
+ * @param {AppendableElementGuts} html
+ * @param {CodeMirror.EditorConfiguration} [options]
+ * @return {HelpPopupElement & {codebox: CodeMirror.Editor}}
+ */
 export function showCodeMirrorPopup(title, html, options) {
   const popup = helpPopup.show(title, html, {className: 'big'});
 

commit 3b4c43aed6f3b74bba6ed52aee167caac0a09673

fix #2073: import was broken by 527e1231

src/manage/import-export.js
@@ -96,7 +96,7 @@ async function importFromFile(file) {
 
 async function importFromString(jsonString) {
   let json = JSON.parse(jsonString) || [];
-  if (json._rev === json.doc?._rev)
+  if (json._rev && json._rev === json.doc?._rev)
     json = [json.doc];
   const oldStyles = Array.isArray(json) && json.length ? await API.styles.getAll() : [];
   const oldStylesSet = new Set(oldStyles.sort((a, b) =>