35b379801718da68e29c7753909f95eb7031a170
tophf <tophff@gmail.com>
2026-06-25 20:29:52 +0300

    mark urlParams as side-effects-free

--- src/js/util.js
+++ src/js/util.js
@@ -30,7 +30,7 @@ export const globAsRegExpStr = s => s.replace(/[{}()[\]\\.+*?^$|]/g, '\\$&')
   .replace(/\\\\\\\*/g, '\\*');
 export const RX_MAYBE_REGEXP = /^\s*\/(.+?)\/([simguy]*)\s*$/;
 export const RX_META = /\/\*!?\s*==userstyle==(?:[^*]+?|\*(?!\/))*==\/userstyle==\s*\*\//i;
-export const urlParams = new URLSearchParams(location.search);
+export const urlParams = /*@__PURE__*/new URLSearchParams(location.search);
 export const isSidebar = /*@__PURE__*/urlParams.has(kSidebar);
 
 const tCache = /*@__PURE__*/new Map();
