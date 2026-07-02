openstyles/stylus

commit 7dccba6de0ebe2f4c77a4f57ee7846dd3ef27ec5

keep css nesting for modern browser targets

postcss.config.js
@@ -1,11 +1,13 @@
 'use strict';
 
-const {getBrowserlist} = require('./tools/util');
+const {getBrowserlist, getBrowserTargets} = require('./tools/util');
+const targets = Object.fromEntries(getBrowserTargets());
 
 module.exports = {
   plugins: [
     'postcss-import',
     'postcss-simple-vars',
+    (targets.chrome < 120 || targets.firefox < 117) && // nesting added in these versions
     'postcss-nested', // see 'nesting-rules' comment
     ['postcss-preset-env', {
       browsers: getBrowserlist(),
@@ -21,5 +23,5 @@ module.exports = {
         'prefers-color-scheme-query': false, // we manually handle it via cssRules
       },
     }],
-  ],
+  ].filter(Boolean),
 };

src/popup/popup.css
@@ -284,10 +284,6 @@ body {
       line-height: var(--entry-h);
       pointer-events: none;
     }
-    html[style*="border"] &::before {
-      /* the "show side borders" option adds 2px */
-      right: 7px;
-    }
     &:hover::before {
       color: var(--c50);
     }

tools/util.js
@@ -42,15 +42,17 @@ function escapeToRe(str, flags) {
   return new RegExp(str.replace(/[{}()[\]\\.+*?^$|]/g, '\\$&'), flags);
 }
 
+function getBrowserTargets() {
+  return [
+    BUILD !== 'firefox' && ['chrome', require(SRC + MANIFEST_OVR).minimum_chrome_version],
+    BUILD !== 'chrome' && ['firefox', require(SRC + MANIFEST_OVR.replace('.', '-firefox.'))
+      .browser_specific_settings.gecko.strict_min_version],
+  ].filter(Boolean);
+}
+
 function getBrowserlist() {
-  return Object.entries({
-    Chrome: BUILD !== 'firefox' &&
-      require(SRC + MANIFEST_OVR).minimum_chrome_version,
-    Firefox: BUILD !== 'chrome' &&
-      require(SRC + MANIFEST_OVR.replace('.', '-firefox.'))
-        .browser_specific_settings.gecko.strict_min_version,
-  }).map(([name, ver]) => ver && `${name} >= ${parseFloat(ver)}`)
-    .filter(Boolean);
+  return getBrowserTargets()
+    .map(([name, ver]) => ver && `${name} >= ${parseFloat(ver)}`);
 }
 
 function transESM2var(buf, from) {
@@ -94,6 +96,7 @@ module.exports = {
   escapeForRe,
   escapeToRe,
   getBrowserlist,
+  getBrowserTargets,
   makePatchOptions,
   nukeHtmlSpaces,
   transESM2var,

commit 613d02be4dd9b75b4244455090c707dc8e3ce01d

fix dark scrollbar broken in 7dccba6d

postcss.config.js
@@ -2,26 +2,32 @@
 
 const {getBrowserlist, getBrowserTargets} = require('./tools/util');
 const targets = Object.fromEntries(getBrowserTargets());
-
-module.exports = {
-  plugins: [
-    'postcss-import',
-    'postcss-simple-vars',
-    (targets.chrome < 120 || targets.firefox < 117) && // nesting added in these versions
-    'postcss-nested', // see 'nesting-rules' comment
-    ['postcss-preset-env', {
-      browsers: getBrowserlist(),
-      features: {
-        'clamp': false, // used intentionally with a fallback
-        'is-pseudo-class': {
-          specificityMatchingName: 'Z',
-        },
-        /** disabling the built-in postcss-nesting plugin because is uses :is() for correctness,
-         * but it requires postcss-is-pseudo-class plugin which emits warnings about our css,
-         * so we use a different postcss-nested plugin that seemingly works just fine. */
-        'nesting-rules': false,
-        'prefers-color-scheme-query': false, // we manually handle it via cssRules
+/** Inverted condition to ignore NaN target when building for a specific target */
+const nesting = !(targets.chrome < 120 || targets.firefox < 117);
+const kPostcssNested = 'postcss-nested';
+const plugins = [
+  'postcss-import',
+  'postcss-simple-vars',
+  !nesting && kPostcssNested, // see 'nesting-rules' comment
+  ['postcss-preset-env', {
+    browsers: getBrowserlist(),
+    features: {
+      'clamp': false, // used intentionally with a fallback
+      'is-pseudo-class': {
+        specificityMatchingName: 'Z',
       },
-    }],
-  ].filter(Boolean),
-};
+      /** disabling the built-in postcss-nesting plugin because is uses :is() for correctness,
+       * but it requires postcss-is-pseudo-class plugin which emits warnings about our css,
+       * so we use a different postcss-nested plugin that seemingly works just fine. */
+      'nesting-rules': false,
+      'prefers-color-scheme-query': false, // we manually handle it via cssRules
+    },
+  }],
+].filter(Boolean);
+const cfg = {plugins};
+const cfgNoNesting = nesting && {plugins: plugins.filter(p => p !== kPostcssNested)};
+
+module.exports = !nesting ? cfg :
+  ctx => ctx.file.endsWith('global-dark.css') // ::-webkit-scrollbar can't be nested
+    ? cfgNoNesting
+    : cfg;