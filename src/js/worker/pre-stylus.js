399eea08b224891bb091b257ee91a44aa55321d7
tophf <tophff@gmail.com>
2026-07-02 11:47:05 +0300

    set stylus vars inline + adjust error lineno

--- src/js/worker/pre-stylus.js
+++ src/js/worker/pre-stylus.js
@@ -3,41 +3,53 @@ import {loadStylusLang, stylusLang} from './util';
 
 /** @type {StyleSection[]} */
 let sectionsTmp;
-let metaStrTmp;
+let metaStrTmp, varsSep, varsUsed;
 
 export default function preStylus(code, metaStr, vars, sections, log, warn) {
   if (!stylusLang)
     loadStylusLang();
-  if (sectionsTmp === undefined)
+  if (!varsSep) {
+    /** Added after vars to ensure any leftovers are removed. Not using `globals` because it has
+     * to use a separate Parser+Evaluator which pre-parses all built-in functions and named colors
+     * which is then performed again wastefully on the main code. */
+    varsSep = 'sep' + Math.random().toString(36).slice(2);
     stylusLang.Compiler.prototype.visitRoot = extractSectionsFromStylus;
+  }
   if (vars) {
-    const str = `@import 'functions/index.styl';\n` +
-      `vars={${Object.keys(vars).map(k => `'${k}':${vars[k].value}`).join(',\n')}}`;
-    const ast = new stylusLang.Parser(str).parse();
-    const ev = new stylusLang.Evaluator(ast).evaluate();
-    vars = ev.nodes[1].nodes[0].vals;
+    vars = Object.keys(vars).map(k => `${k}=${vars[k].value};\n`).join('') +
+      '@' + varsSep + ';\n';
+    code = vars + code;
   }
   metaStrTmp = metaStr;
   sectionsTmp = sections;
-  code = stylusLang(code, {
+  varsUsed = !!vars;
+  stylusLang(code, {
     /** Copied from postcss-styl to avoid it crashing due to an empty lexer.
      *  TODO: see if this noticeably reduces performance and maybe patch postcss-styl. */
     cache: false,
-    globals: vars,
     functions: {
       p: node => log.push(node.val || node) && stylusLang.nodes.null,
       warn: node => warn.push(node.val || node) && stylusLang.nodes.null,
     },
-  }).render();
+  }).render((err, css) => {
+    if (err) {
+      if (vars) err.message = err.message.replace(/:(\d+)(?=:)/,
+        // Not using Object.keys(vars).length because values may have \n inside
+        (s, line) => ':' + (line - vars.match(/^/gm).length - 1));
+      throw err;
+    }
+    code = css;
+  });
   metaStrTmp = sectionsTmp = null;
   return code;
 }
 
 function extractSectionsFromStylus(block) {
-  let cmt, k, v;
+  let cmt, k, v, sepSkipped;
   this.buf = '';
   for (const node of block.nodes) {
     if ((v = node.str) && v !== metaStrTmp && v.charCodeAt(0) === 47/* / */ && (cmt = v)
+    || varsUsed && !sepSkipped && (node.type !== varsSep || (sepSkipped = true))
     || node.suppress)
       continue;
     if (node.type === '-moz-document') {
